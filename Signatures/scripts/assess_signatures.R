a79c0c82e7ee3bd226ae3abe68de5d358c421353
julianguyn <julianguyenpt@gmail.com>
2024-04-19 23:43:23 -0400

    minor changes

--- Signatures/scripts/assess_signatures.R
+++ Signatures/scripts/assess_signatures.R
@@ -12,55 +12,21 @@ pal <- c("Signature1" = "#046C9A", "Signature2" = "#BBADB9", "Signature3" = "#72
         "Signature4" = "#E8E1D9", "Signature5" = "#AFC5D8", "Signature6" = "#DF9C93")
 
 
-# load in TCGA binary matrix
-TCGA <- read.table("Signatures/data/BCa_binary.2.matrix", header = T)
-nmf_scores <- read.table("Signatures/results/ATAC_heatmap_rank6.png.order.matrix", header = T)
+# load in TCGA cell line binary matrix
+mat <- read.table("Signatures/data/BM_tcgabca_bcacelllines.tsv", header = T)
 
-# load in cell line binary matrix
-cells <- read.table("Signatures/data/bcacell_lines.tsv", header = T)
+# load in TCGA binary matrix
+nmf_scores <- read.table("Signatures/results/data/ATAC_heatmap_rank6.png.order.matrix", header = T)
 
 # extract consensus sequences
-consensus <- TCGA[,1:3]
-cell_peaks <- cells[,1:3]
-
-# find overlap in peak calls
-consensus$merge <- paste0(consensus$V1, "-", consensus$V2, "-", consensus$V3)
-cell_peaks$merge <- paste0(cell_peaks$V1, "-", cell_peaks$V2, "-", cell_peaks$V3)
-
-consensus$cells <- 0
-
-# subset by chromosome for quicker search
-subset <- cell_peaks[cell_peaks$V1 == "chr1",]
-
-for (i in 1:nrow(consensus)) {
-    
-    # get new subset if chr changes
-    if (subset$V1[1] != consensus$V1[i]) {
-        print(paste("---------starting", consensus$V1[i]))
-        subset <- cell_peaks[cell_peaks$V1 == consensus$V1[i], ]
-    }
-    if (i %% 5000 == 0) {print(paste(i, "out of", 627305))} 
-
-    if (consensus$merge[i] %in% subset$merge) {
-
-        # find direct overlap
-        consensus$cells[i] <- subset$merge[i] 
-
-    } else {
-
-        # find the closest peak
-        subset$diff <- abs(subset$V2 - consensus$V2[i]) + abs(subset$V3 - consensus$V3[i])
-        closest <- subset$merge[which.min(subset$diff)]
-        consensus$cells[i] <- closest
-    }
-}
-print("saving")
-save(consensus, file = "Signatures/results/TCGA_cell_match.RData")
+consensus <- mat[,1:3]
+mat <- mat[,-c(1:3)]
 
 ## TODO: Remove Lucie duplicates
 # load in BCa cell lines
-samples <- read.csv("DrugResponse/data/cl_samples.csv")
-samples <- samples[match(umap_df$sample[76:138], samples$file),]
+#samples <- read.csv("DrugResponse/data/cl_samples.csv")
+#samples$file <- gsub("^X", "", gsub("\\.(?!$)", "-", samples$file, perl = TRUE))
+#samples <- samples[match(umap_df$sample[76:138], samples$file),]
 # TEMP
 # dup <- samples$sample[duplicated(samples$sample)]
 # tmp <- samples[samples$sample %in% dup,]
@@ -82,42 +48,32 @@ samples <- samples[match(umap_df$sample[76:138], samples$file),]
 #     labs(x = "UMAP1", y = "UMAP2", shape = "Type", fill = "Signature")
 #dev.off()
 
-# reorder cell peak calls to TCGA consensus sequence
-cells_matched <- cells[match(consensus$cells, cell_peaks$merge),]
-rownames(cells_matched) <- NULL
-
-# combine TCGA tumour and cell binary files
-TCGA <- TCGA[,-c(1:3)]
-cells_matched <- cells_matched[,-c(1:3)]
-
-combined <- cbind(TCGA, cells_matched)
-
 # extract signatures
 nmf_scores$Signature <- paste0("Signature", 1:6)
 nmf_scores <- melt(nmf_scores)
 nmf_scores <- as.data.frame(nmf_scores %>% group_by(variable) %>% filter(value == max(value)) %>% ungroup())
-tcga_signatures <- nmf_scores[match(colnames(TCGA), nmf_scores$variable),]$Signature
+tcga_signatures <- nmf_scores[match(colnames(mat)[1:75], nmf_scores$variable),]$Signature
 
 # transform matrix
-combined <- t(combined)
-combined[is.na(combined)] <- 0
-sample_names <- rownames(combined)
+mat <- t(mat)
+mat[is.na(mat)] <- 0
+sample_names <- rownames(mat)
 
 # create UMAP projection
-umap_df <- umap(combined)
-umap_df <- as.data.frame(umap_df$layout)
+umap_df <- as.data.frame(umap(mat)$layout)
+save(umap_df, file = "Signatures/results/data/umap.RData")
 
 # formating umap df
 colnames(umap_df) <- c("UMAP1", "UMAP2")
-umap_df$Signature <- c(tcga_signatures, rep("", ncol(cells_matched)))
-umap_df$type <- c(rep("Tumour", ncol(TCGA)), rep("Cell Line", ncol(cells_matched)))
+umap_df$Signature <- c(tcga_signatures, rep("", 63))
+umap_df$type <- c(rep("Tumour", 75), rep("Cell Line", 63))
 umap_df$type <- factor(umap_df$type, levels = c("Tumour", "Cell Line"))
 umap_df$sample <- sample_names
 
 save(umap_df, file = "Signatures/results/umap_df.RData")
 
-# plot umap
-png("Signatures/results/signature_umap.png", width = 5, height = 4, res = 600, units = "in")
+# plot umap by signature
+png("Signatures/results/figures/ATAC_umap_signature.png", width = 5, height = 4, res = 600, units = "in")
 ggplot(data = umap_df, aes(x = UMAP1, y = UMAP2, shape = type)) + 
     geom_point(data = umap_df[umap_df$type == "Tumour",], aes(fill = Signature), size = 4, alpha = 0.8) +
     geom_point(data = umap_df[umap_df$type == "Cell Line",], fill = "#415A77", size = 2) +
@@ -129,5 +85,47 @@ ggplot(data = umap_df, aes(x = UMAP1, y = UMAP2, shape = type)) +
     theme_classic() + 
     theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5), legend.key.size = unit(0.7, 'cm')) +
     labs(x = "UMAP1", y = "UMAP2", shape = "Type", fill = "Signature")
+dev.off()
+
+
+
+
+##### label by subtype
+
+# set up palette for plotting
+subtype_pal <- c("Basal" = "#394032", "Her2" = "#A6A57A","LumA" = "#8C5E58","LumB" = "#5A352A","Normal" = "#8F8073", "#eFeBF7")
+
+
+# load in TCGA annotation
+meta <- read.csv("Signatures/data/TCGA_subtype_label.csv")
+tumours <- gsub("\\.", "-", gsub("X", "", rownames(umap_df)[1:75]))
+meta <- meta[match(tumours, meta$File.Name),]
+
+# load in cell line annotation
+cells_meta <- read.csv("MetaData/Lupien/BCa_samples.csv")
+# from https://bmcgenomics.biomedcentral.com/articles/10.1186/s12864-016-2911-z/tables/3, set 600MPE to LuminalA
+cells_meta[which(cells_meta$subtype == "cell_line"),]$subtype <- "LumA"
+
+# keep only cell lines being used
+samples <- read.csv("DrugResponse/data/cl_samples.csv")
+samples$match <- gsub("^X", "", gsub("\\.", "-", gsub("\\.$", "", samples$file)))
+cells_meta <- cells_meta[cells_meta$sample %in% samples$match,]
+cells <- rownames(umap_df)[76:138]
+samples <- samples[match(cells, samples$file),]
+
+# get subtypes
+samples$subtype <- cells_meta[match(samples$match, cells_meta$sample),]$subtype
+umap_df$subtype <- c(meta$bca_subtype, samples$subtype)
+
+
+# plot umap by subtype
+png("Signatures/results/figures/ATAC_umap_subtype.png", width = 5, height = 4, res = 600, units = "in")
+ggplot(data = umap_df, aes(x = UMAP1, y = UMAP2, shape = type, fill = subtype)) + geom_point(size = 3) +
+    scale_shape_manual(values = c(22, 21)) +
+    scale_fill_manual(values = c(subtype_pal)) +
+    guides(shape = guide_legend(title = "Type", ncol = 1), fill = guide_legend(override.aes=list(shape = 22))) +
+    theme_classic() + 
+    theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5), legend.key.size = unit(0.7, 'cm')) +
+    labs(x = "UMAP1", y = "UMAP2", shape = "Type", fill = "Subtype")
 dev.off()
 
--- Signatures/scripts/make_heatmap.R
+++ Signatures/scripts/make_heatmap.R
@@ -9,10 +9,6 @@ library(ggh4x)
 library(ggpubr)
 suppressMessages(library(grid))
 suppressMessages(library(gridExtra))
-suppressMessages(library(grid))
-suppressMessages(library(gridExtra))
-
-setwd("C:/Users/julia/Documents/BCaATAC")
 
 
 # set up palette for plotting
@@ -20,20 +16,21 @@ pal <- c("Signature1" = "#046C9A", "Signature2" = "#BBADB9", "Signature3" = "#72
         "Signature4" = "#E8E1D9", "Signature5" = "#AFC5D8", "Signature6" = "#DF9C93",
         "Signature7" = "#4E4C67", "Signature8" = "#B4869F", "Signature9" = "#A6B1E1",
         "Signature10" = "#985F6F")
-subtype_pal <- c("#394032", "#A6A57A", "#8C5E58", "#5A352A", "#8F8073", "#eFeBF7")
+subtype_pal <- c("Basal" = "#394032","Her2" = "#A6A57A", "LumA" = "#8C5E58", 
+                 "LumB" = "#5A352A", "Normal" = "#8F8073", "Not Available" = "#eFeBF7")
+
+# read in meta data
+meta <- read.csv("Signatures/data/TCGA_subtype_label.csv")
+
 
 # load in matrix file from NMF
-mat <- read.table("Signatures/results/ATAC_heatmap_rank6.png.order.matrix", header = T)
+mat <- read.table("Signatures/results/data/ATAC_heatmap_rank6.png.order.matrix", header = T)
 
 # get signature
 mat$Signature <- paste0("Signature", 1:6)
 
 # format for plotting
 mat <- melt(mat)
-
-# read in meta data
-meta <- fread("Signatures/data/brca.assignment.share.txt")
-meta <- meta[1:75]
 
 # save molecular subtype
 meta$File.Name <- gsub("-", "\\.", meta$File.Name)
@@ -50,21 +47,22 @@ for (sample in mat$variable) {
 
 
 # reorder signatures
-mat$Signature <- factor(mat$Signature, levels = paste0("Signature",6:1))
+mat$Signature <- paste0(mat$Signature, " ")
+mat$Signature <- factor(paste0(mat$Signature, " "), levels = paste0("Signature",6:1," "))
 
 
 
 ### Plot Heatmap Rank 6 ###
-p1 <- ggplot(mat, aes(x = variable, y = 1, fill = subtype)) + geom_tile(color = "black") +
+p1 <- ggplot(mat, aes(x = variable, y = 1, fill = subtype)) + geom_tile(color = "gray") +
     theme_void() + scale_fill_manual("BCa\nSubtype", values = subtype_pal,
                     labels = c("Basal", "Her2", "LuminalA", "LuminalB", "Normal", "NA")) +
-    theme(axis.title.y = element_text(size=12)) + labs(y = "                ")
+    theme(axis.title.y = element_text(size=12)) + labs(y = "                 ")
 
-p2 <- ggplot(mat, aes(x = variable, y = 1, fill = signature_assign)) + geom_tile(color = "black") +
+p2 <- ggplot(mat, aes(x = variable, y = 1, fill = signature_assign)) + geom_tile(color = "gray") +
     theme_void() + scale_fill_manual("Assigned\nSignature", values = pal) +
-    theme(axis.title.y = element_text(size=12)) + labs(y = "                ")
+    theme(axis.title.y = element_text(size=12)) + labs(y = "                 ")
 
-p3 <- ggplot(mat, aes(x = variable, y = Signature, fill = value)) + geom_tile(color = "black") +
+p3 <- ggplot(mat, aes(x = variable, y = Signature, fill = value)) + geom_tile(color = "gray") +
     scale_fill_gradientn("Signature    \nScore", colours = brewer.pal(9, "Blues")) + theme_void() +
     theme(axis.text.y = element_text(size=11), axis.title.x = element_text(size=12)) + labs(x = "Tumour Sample")
 
@@ -76,7 +74,101 @@ p1 <- p1+theme(legend.position = "none")
 p2 <- p2+theme(legend.position = "none")
 p3 <- p3+theme(legend.position = "none")
 
-png("Signatures/results/rank6_heatmap.png", width = 10, height = 4, res = 600, units = "in")
+png("Signatures/results/figures/ATAC_rank6_heatmap.png", width = 11, height = 4, res = 600, units = "in")
+grid.arrange(p1, p2, p3, l1, l2, l3, ncol = 9, nrow = 12,
+    layout_matrix = rbind(c(1,1,1,1,1,1,1,4,5), c(2,2,2,2,2,2,2,4,5),
+                          c(3,3,3,3,3,3,3,4,5), c(3,3,3,3,3,3,3,4,5),
+                          c(3,3,3,3,3,3,3,4,5), c(3,3,3,3,3,3,3,4,5),
+                          c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,6,NA),
+                          c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,6,NA),
+                          c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,NA,NA)))
+dev.off()
+
+
+
+#### plotting for RNA signatures
+read_mat <- function(file, rank) {
+        
+    # load in matrix file from NMF
+    mat <- read.table(file, header = T)
+
+    # get signature
+    mat$Signature <- paste0("Signature", 1:rank)
+
+    # format for plotting
+    mat <- melt(mat)
+
+    # save molecular subtype
+    subtype <- c()
+    for (sample in mat$variable) {subtype <- c(subtype, meta[meta$sample_name == sample,]$bca_subtype[1])}
+    mat$subtype <- subtype
+
+    # save signature assignment
+    mat$signature_assign <- ""
+    for (sample in mat$variable) {
+        tmp <- mat[mat$variable == sample,]
+        mat[mat$variable == sample,]$signature_assign <- as.character(tmp[which.max(tmp$value),]$Signature)
+    }
+
+    # reorder signatures
+    mat$Signature <- factor(mat$Signature, levels = paste0("Signature",rank:1))
+
+    return(mat)
+}
+
+
+
+### Plot Heatmap Rank 6 ###
+mat <- read_mat("Signatures/results/data/RNA_heatmap_rank6.png.order.matrix", 6)
+
+
+### Plot Heatmap Rank 6 ###
+p1 <- ggplot(mat, aes(x = variable, y = 1, fill = subtype)) + geom_tile(color = "gray") +
+    theme_void() + scale_fill_manual("BCa\nSubtype", values = subtype_pal,
+                    labels = c("Basal", "Her2", "LuminalA", "LuminalB", "Normal", "NA")) +
+    theme(axis.title.y = element_text(size=12)) + labs(y = "                 ")
+
+p2 <- ggplot(mat, aes(x = variable, y = 1, fill = signature_assign)) + geom_tile(color = "gray") +
+    theme_void() + scale_fill_manual("Assigned\nSignature", values = pal) +
+    theme(axis.title.y = element_text(size=12)) + labs(y = "                 ")
+
+p3 <- ggplot(mat, aes(x = variable, y = Signature, fill = value)) + geom_tile(color = "gray") +
+    scale_fill_gradientn("Signature    \nScore", colours = brewer.pal(9, "Blues")) + theme_void() +
+    theme(axis.text.y = element_text(size=11), axis.title.x = element_text(size=12)) + labs(x = "Tumour Sample")
+
+# extract legends
+l1 <- as_ggplot(get_legend(p1))
+l2 <- as_ggplot(get_legend(p2))
+l3 <- as_ggplot(get_legend(p3))
+p1 <- p1+theme(legend.position = "none")
+p2 <- p2+theme(legend.position = "none")
+p3 <- p3+theme(legend.position = "none")
+
+png("Signatures/results/figures/RNA_rank6_heatmap.png", width = 11, height = 4, res = 600, units = "in")
+grid.arrange(p1, p2, p3, l1, l2, l3, ncol = 9, nrow = 12,
+    layout_matrix = rbind(c(1,1,1,1,1,1,1,4,5), c(2,2,2,2,2,2,2,4,5),
+                          c(3,3,3,3,3,3,3,4,5), c(3,3,3,3,3,3,3,4,5),
+                          c(3,3,3,3,3,3,3,4,5), c(3,3,3,3,3,3,3,4,5),
+                          c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,6,NA),
+                          c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,6,NA),
+                          c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,NA,NA)))
+dev.off()
+
+
+
+p1 <- make_p1(mat6)
+p2 <- make_p2(mat6)
+p3 <- make_p3(mat6)
+
+# extract legends
+l1 <- as_ggplot(get_legend(p1))
+l2 <- as_ggplot(get_legend(p2))
+l3 <- as_ggplot(get_legend(p3))
+p1 <- p1+theme(legend.position = "none")
+p2 <- p2+theme(legend.position = "none")
+p3 <- p3+theme(legend.position = "none")
+
+png("Signatures/results/figures/RNA_rank6_heatmap.png", width = 10, height = 4, res = 600, units = "in")
 grid.arrange(p1, p2, p3, l1, l2, l3, ncol = 9, nrow = 13,
     layout_matrix = rbind(c(1,1,1,1,1,1,1,4,5),
                         c(2,2,2,2,2,2,2,4,5),
