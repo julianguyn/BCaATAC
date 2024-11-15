67a57e72094b80c49ef2c974a591593439e2760b
julianguyn <julianguyenpt@gmail.com>
2024-11-14 23:26:53 -0500

    fix: make CI computation more efficient

--- SupervisedSignatures/scripts/feature_reduction.R
+++ SupervisedSignatures/scripts/feature_reduction.R
@@ -2,32 +2,11 @@
 
 setwd("C:/Users/julia/Documents/BCaATAC")
 
-library(data.table)
-suppressMessages(library(readxl))
-suppressMessages(library(survcomp))
-
-### Identify drugs of interest ###
-
-# read in PDX drug response data
-pdx_response <- as.data.frame(read_excel("DrugResponsePDX/data/drugresponse/DrugResponse_PDX.xlsx", sheet = 1))
-pdx_response[pdx_response == "NA"] <- NA
-pdx_response[pdx_response == "TAXOL"] <- "PACLITAXEL"
-pdx_response[pdx_response == "CARBOPLATIN"] <- "CARBOPLATINUM"
-pdx_response <- na.omit(pdx_response)
-
-# remove CONTROLs and H2O samples (left with n=23 drugs)
-drugs_of_interest <- unique(pdx_response$drug[-which(pdx_response$drug %in% c("H2O", pdx_response$drug[grep("CONTROL", pdx_response$drug)]))])
-pdx_response <- pdx_response[pdx_response$drug %in% drugs_of_interest,]
-
-# keep only drugs with at least 10 samples (left with n=11 drugs)
-drugs_of_interest <- names(table(pdx_response$drug)[table(pdx_response$drug) > 10])
-pdx_response <- pdx_response[pdx_response$drug %in% drugs_of_interest,]
-
-
-### Get cell line drug response for drugs of interest ### 
-
-# load in cell line drug response data
-load("DrugResponse/results/data/sensitivity_data.RData")
+# load libraries
+suppressPackageStartupMessages({
+    library(data.table)
+    library(survcomp)
+})
 
 # load in and extract needed cell lines
 samples <- read.csv("DrugResponse/data/cl_samples.csv")
@@ -35,107 +14,93 @@ dup <- samples$sample[duplicated(samples$sample)]
 tmp <- samples[samples$sample %in% dup,]
 samples <- rbind(samples[-which(samples$sample %in% dup),], tmp[which(tmp$seq == "Nergiz"),]) # from dups, keep only the ones by Nergiz
 
-
-# keep only drugs of interest
-ubr1_sen <- ubr1_sen[toupper(rownames(ubr1_sen)) %in% drugs_of_interest,colnames(ubr1_sen) %in% samples$sample] #taxol only, 43 cells
-ubr2_sen <- ubr2_sen[toupper(rownames(ubr2_sen)) %in% drugs_of_interest,colnames(ubr2_sen) %in% samples$sample] #taxol, carboplatin, & eribulin, 42 cells
-gray_sen <- gray_sen[toupper(rownames(gray_sen)) %in% drugs_of_interest,colnames(gray_sen) %in% samples$sample] #taxol & carboplatin, 41 cells
-gcsi_sen <- gcsi_sen[toupper(rownames(gcsi_sen)) %in% drugs_of_interest,colnames(gcsi_sen) %in% samples$sample] #taxol only, 25 cells
-gdsc_sen <- gdsc_sen[toupper(rownames(gdsc_sen)) %in% drugs_of_interest,colnames(gdsc_sen) %in% samples$sample] #taxol only, 34 cells
-ctrp_sen <- ctrp_sen[toupper(rownames(ctrp_sen)) %in% drugs_of_interest,colnames(ctrp_sen) %in% samples$sample] #taxol & carboplatin, 32 cells
-ccle_sen <- ccle_sen[toupper(rownames(ccle_sen)) %in% drugs_of_interest,colnames(ccle_sen) %in% samples$sample] #taxol only, 23 cells
-
-
-# save to H4H 
-# save(samples, ubr1_sen, ubr2_sen, gray_sen, gcsi_sen, gdsc_sen, ctrp_sen, ccle_sen, file = "temp_toH4H.RData")
-
-######## TEMPORARY, FOR H4H
-library(data.table)
-suppressMessages(library(survcomp))
-
-load("../temp_toH4H.RData")
-##############################
-
-# colnames of sensitivity has to match with peaks
-
-# load in peak calls
-peak_calls <- as.data.frame(fread("../bcacell_lines.tsv"))
-rownames(peak_calls) <- paste(peak_calls$V1, peak_calls$V2, peak_calls$V3, sep = ":")
-peak_calls <- peak_calls[,-c(1:3)]
+# load in binary peak matrix
+peak_mat <- fread("Signatures/data/bcacell_lines.tsv") |> as.data.frame()
+rownames(peak_mat) <- paste(peak_mat$V1, peak_mat$V2, peak_mat$V3, sep = ":")
+peak_mat <- peak_mat[,-c(1:3)]
 
 # keep only needed samples and rename sample names
-colnames(peak_calls) <- gsub("-", "\\.", colnames(peak_calls))
-peak_calls <- peak_calls[,which(colnames(peak_calls) %in% samples$file)]
-colnames(peak_calls) <- samples[match(colnames(peak_calls), samples$file),]$sample
+colnames(peak_mat) <- gsub("-", "\\.", colnames(peak_mat))
+peak_mat <- peak_mat[,which(colnames(peak_mat) %in% samples$file)]
+colnames(peak_mat) <- samples[match(colnames(peak_mat), samples$file),]$sample
 
 
-### Compute concordance index to find univariable associations ###
+### ===== Compute concordance index to find univariable associations ===== ###
 
 # function to compute concordance index
 computeCI <- function(signature_scores, sensitivity_data, label) {
 
+    # keep and order common cell lines
+    common_cells <- intersect(colnames(signature_scores), colnames(sensitivity_data))
+    signature_scores <- signature_scores[,common_cells]
+    sensitivity_data <- sensitivity_data[,common_cells]
+
     # create data frame to hold results
-    combinations <- as.data.frame(matrix(data = NA, nrow = nrow(signature_scores) * nrow(sensitivity_data), ncol = 7))
-    colnames(combinations) <- c("peak", "drug", "ci", "pvalue", "se", "upper", "lower")
-    combinations$peak <- rep(rownames(signature_scores), nrow(sensitivity_data))
-    combinations$drug <- rep(rownames(sensitivity_data), each = nrow(signature_scores))
+    combinations <- expand.grid(peak = rownames(signature_scores), drug = rownames(sensitivity_data))
+    num_combinations <- nrow(combinations)
 
-    # compute concordance index
-    for (i in 1:nrow(combinations)){
-        if (i %% 10000 == 0) {print(paste0(i, " out of ", nrow(combinations), " complete"))}
+    # initialize columns to store univariable results
+    combinations$ci <- NA
+    combinations$pvalue <- NA
+    combinations$se <- NA
+    combinations$upper <- NA
+    combinations$lower <- NA
 
-        ci <- survcomp::concordance.index(as.numeric(sensitivity_data[combinations[,2][i],]), 
-                                            # row from sensitivity_data with the drug for all samples
-                                            surv.time = as.numeric(unlist(-signature_scores[combinations[,1][i],])), 
-                                            # row of signature i in signature scores matrix with cell lines as columns
-                                            surv.event = rep(1,length(sensitivity_data)), 
-                                            # df of all drugs as rows with all samples as columns
-                                            outx = TRUE, method="noether", na.rm = TRUE)
+    # extract drug and feature data into lists
+    sen_list <- lapply(rownames(sensitivity_data), function(drug) as.numeric(sensitivity_data[drug, ]))
+    feat_list <- lapply(rownames(signature_scores), function(peak) as.numeric(-signature_scores[peak, ]))
+    num_samples <- length(sen_list[[1]])
 
-        combinations$pvalue[i] <- ci$p.value
+    # compute concordance index for each feature-drug pair
+    for (i in 1:num_combinations) {
+        if (i %% 10000 == 0) { print(paste0(i, " out of ", num_combinations, " complete"))}
+        
+        # compute concordance index
+        ci <- survcomp::concordance.index(
+            sen_list[[which(rownames(sensitivity_data) == combinations$drug[i])]],                  # sensitivity data for all samples
+            surv.time = feat_list[[which(rownames(signature_scores) == combinations$peak[i])]],     # feature values for all samples
+            surv.event = rep(1, num_samples),
+            outx = TRUE,
+            method = "noether",
+            na.rm = TRUE
+        )
+
         combinations$ci[i] <- ci$c.index
+        combinations$pvalue[i] <- ci$p.value
         combinations$se[i] <- ci$se
         combinations$upper[i] <- ci$upper
         combinations$lower[i] <- ci$lower
-
-        
     }
 
     # filtering and multiple test correction
     combinations <- combinations[complete.cases(combinations$pvalue),]
-    combinations$FDR <- p.adjust(combinations$pvalue, method = "BH", n = length(combinations$pvalue))
-    combinations$FDRsig <- ifelse(combinations$FDR < 0.05, TRUE, FALSE)
+    combinations$FDR <- p.adjust(combinations$pvalue, method = "BH")
+    combinations$FDRsig <- combinations$FDR < 0.05
 
-    
     # format dataframe for plotting (order by ci and add variable rank)
     combinations <- combinations[order(combinations$ci),]
-    combinations$rank <- 1:nrow(combinations)
+    combinations$rank <- seq_len(nrow(combinations))
     combinations$pairs <- paste0(combinations$peak, "-", combinations$drug)
-    combinations$pset <- c(rep(label, nrow(combinations)))
-    
+    combinations$pset <- label
+
     return(combinations)
 }
 
-# subset peak calls to samples in dataset
-ubr1_sig <- peak_calls[,which(colnames(peak_calls) %in% colnames(ubr1_sen))]
-ubr2_sig <- peak_calls[,which(colnames(peak_calls) %in% colnames(ubr2_sen))]
-gray_sig <- peak_calls[,which(colnames(peak_calls) %in% colnames(gray_sen))]
-gcsi_sig <- peak_calls[,which(colnames(peak_calls) %in% colnames(gcsi_sen))]
-gdsc_sig <- peak_calls[,which(colnames(peak_calls) %in% colnames(gdsc_sen))]
-ctrp_sig <- peak_calls[,which(colnames(peak_calls) %in% colnames(ctrp_sen))]
-ccle_sig <- peak_calls[,which(colnames(peak_calls) %in% colnames(ccle_sen))]
+# subset to compute only paclitaxel
+ubr2_sen <- ubr2_sen["Paclitaxel",]
+gray_sen <- gray_sen["Paclitaxel",]
+ctrp_sen <- ctrp_sen["Paclitaxel",]
 
 # compute associations
-ubr1_com <- computeCI(ubr1_sig, ubr1_sen, "uhnbreast1")
-ubr2_com <- computeCI(ubr2_sig, ubr2_sen, "uhnbreast2")
-gray_com <- computeCI(gray_sig, gray_sen, "gray")
-gcsi_com <- computeCI(gcsi_sig, gcsi_sen, "gcsi")
-gdsc_com <- computeCI(gdsc_sig, gdsc_sen, "gdsc")
-ctrp_com <- computeCI(ctrp_sig, ctrp_sen, "ctrp")
-ccle_com <- computeCI(ccle_sig, ccle_sen, "ccle")
+ubr1_com <- computeCI(peak_mat, ubr1_sen, "UHNBreast1")
+ubr2_com <- computeCI(peak_mat, ubr2_sen, "UHNBreast2")
+gray_com <- computeCI(peak_mat, gray_sen, "GRAY")
+gcsi_com <- computeCI(peak_mat, gcsi_sen, "gCSI")
+gdsc_com <- computeCI(peak_mat, gdsc_sen, "GDSC")
+ctrp_com <- computeCI(peak_mat, ctrp_sen, "CTRP")
+ccle_com <- computeCI(peak_mat, ccle_sen, "CCLE")
 
-
-
+# save results
 save(ubr1_com, file = "../results/res1.RData")
 save(ubr2_com, file = "../results/res2.RData")
 save(gray_com, file = "../results/res3.RData")
@@ -147,3 +112,38 @@ save(ccle_com, file = "../results/res7.RData")
 # save(ubr1_com, ubr2_com, gray_com, gcsi_com, gdsc_com, ctrp_com, ccle_com, file = "SupervisedDrugResponseAssociations.RData")
 
 
+
+### ===== Remove correlated peaks ===== ###
+
+# on H4H: /cluster/projects/bhklab/projects/BCaATAC/SupervisedSignatures/procdata/peak-counts
+
+# compute correlation matrix
+peak_mat <- t(peak_mat)
+dim(peak_mat) # 49     1224557
+corr_mat <- cor(peak_mat, use = "pairwise.complete.obs")        # current dim: # 49     1224557
+save(corr_mat, file = "corr-mat.RData")
+
+
+upper_tri <- upper.tri(corr_mat)
+
+# set threshold
+thres <- 0.8
+correlated <- which(upper_tri & abs(corr_mat) > thres, arr.ind = TRUE)
+
+# remove correlated peak from identified pairs
+peak_reduced <- peak_mat[-unique(correlated[,2]), ]
+
+# Check the new dimensions of the matrix
+dim(peak_reduced)
+
+save(corr_mat, file = "corr-mat.RData")
+
+
+
+cd /cluster/projects/bhklab/projects/BCaATAC/SupervisedSignatures/scripts/scripts
+# running:
+# ci1.R
+# ci7.R
+
+
+# need to run:
