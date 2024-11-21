7b6ecd544b9bb171ed2abfd239ea9a08d227bc2c
julianguyn <julianguyenpt@gmail.com>
2024-11-21 17:02:30 -0500

    feat: peak annotation

--- SupervisedSignatures/scripts/feature_importance.R
+++ SupervisedSignatures/scripts/feature_importance.R
@@ -5,7 +5,31 @@ setwd("C:/Users/julia/Documents/BCaATAC")
 # load libraries
 suppressPackageStartupMessages({
     library(ggplot2)
+    library(ChIPseeker)
+    library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+    library(GenomicRanges)
+    library(RColorBrewer)
+    library(tidyverse)
+    library(reshape2)
 })
+
+# load in annotation 
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+# define function for peak annotation
+peak_anno <- function(features) {
+    # extract feature information
+    features <- features %>%
+        separate(Peak, into = c("chr", "start", "end"), sep = ":", convert = TRUE)
+
+    # create GRanges object
+    gr <- GRanges(seqnames = features$chr, ranges = IRanges(features$start, features$end))
+
+    # annotate peaks
+    peakAnno <- annotatePeak(gr, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+    return(peakAnno)
+}
 
 ### ===== Feature Importance of Elastic Net ===== ###
 
@@ -32,6 +56,13 @@ ggplot(en_feat, aes(x = Peak, y = abs(Weight), fill = ifelse(Weight > 0, "Positi
 dev.off()
 
 
+# plot peak distribution
+png("SupervisedSignatures/results/figures/elasticnet_peakAnno.png", width = 6, height = 5, res = 600, units = "in")
+plotAnnoPie(peak_anno(en_feat))
+dev.off()
+
+
+
 ### ===== Feature Importance of LASSO ===== ###
 
 # load in feature importance
@@ -54,4 +85,10 @@ ggplot(ls_feat, aes(x = Peak, y = abs(Weight), fill = ifelse(Weight > 0, "Positi
     scale_fill_manual(values = c("#7294D4", "#BC4749"), limits = c("Positive", "Negative")) +
     labs(fill = "Association", y = "Coefficient Magnitude") + 
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), legend.key.size = unit(0.7, 'cm')) 
+dev.off()
+
+
+# plot peak distribution
+png("SupervisedSignatures/results/figures/lasso_peakAnno.png", width = 6, height = 5, res = 600, units = "in")
+plotAnnoPie(peak_anno(ls_feat))
 dev.off()
