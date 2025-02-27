f4d47cb5a97786fa37aa666ba4d804988563c94a
julianguyn <julianguyenpt@gmail.com>
2025-02-26 22:06:56 -0500

    refactor: code style

--- DrugResponse/scripts/indiv_PSet_CI.R
+++ DrugResponse/scripts/indiv_PSet_CI.R
@@ -1,21 +1,23 @@
 setwd("C:/Users/julia/Documents/BCaATAC")
 
-suppressMessages(library(PharmacoGx))
-suppressMessages(library(survcomp))
-library(wesanderson)
-library(ggplot2)
-library(ggh4x)
-library(reshape2)
+# load libraries
+suppressPackageStartupMessages({
+    library(PharmacoGx)
+    library(survcomp)
+    library(wesanderson)
+    library(ggplot2)
+    library(ggh4x)
+    library(reshape2)
+})
+
+###########################################################
+# Load in data
+###########################################################
 
 # load in BCa cell lines
 samples <- read.csv("DrugResponse/data/cl_samples.csv")
 
-# set up palette for plotting
-pal <- c("Signature1" = "#046C9A", "Signature2" = "#BBADB9", "Signature3" = "#7294D4", 
-        "Signature4" = "#E8E1D9", "Signature5" = "#AFC5D8", "Signature6" = "#DF9C93")
-pal2 <- c("#899DA4", "#BC4749")
-
-# get signature scores
+# load in signature
 signature_scores <- read.table("Signatures/data/bca_sign.Zscore.txt")
 # remove duplicates
 dup <- samples$sample[duplicated(samples$sample)]
@@ -24,12 +26,21 @@ samples <- rbind(samples[-which(samples$sample %in% dup),], tmp[which(tmp$seq ==
 
 signature_scores <- signature_scores[,which(colnames(signature_scores) %in% samples$file)]
 
+
+###########################################################
+# Initiate dataframe to store signature associations
+###########################################################
+
 # data frame to keep track of number of significant associations
 sig_com <- as.data.frame(matrix(ncol = 7, nrow = 0))
 colnames(sig_com) <- c("pset", "signature", "drug", "ci", "pvalue", "FDR", "label")
 
 
-### Function to compute CI ###
+###########################################################
+# Define functions to compute concordance index
+###########################################################
+
+# function to compute CI
 computeCI <- function(signature_scores, sensitivity_data, label) {
 
     # create data frame to hold results
@@ -74,6 +85,7 @@ computeCI <- function(signature_scores, sensitivity_data, label) {
     return(combinations)
 }
 
+# function to save significant associations
 saveSig <- function(sig_com, combinations, label) {
 
     # save signatures associated with drug sensitivity:
@@ -89,35 +101,19 @@ saveSig <- function(sig_com, combinations, label) {
     return(sig_com)
 }
 
-########################
-### UHN Breast PSet1 ###
-########################
 
-# load in UHN Breast PSet
-uhnbreast <- readRDS("DrugResponse/data/PSet_UHNBreast.rds")
-uhnbreast <- updateObject(uhnbreast)
+###########################################################
+# Load in PSet sensitivity
+###########################################################
 
-# get drug sensitivity data
+# UHN Breast PSet
+uhnbreast <- readRDS("DrugResponse/data/PSet_UHNBreast.rds") |> updateObject()
 sensitivity_data <- as.data.frame(summarizeSensitivityProfiles(uhnbreast, sensitivity.measure = "aac_recomputed",  fill.missing = F))
 ubr1_sen <- sensitivity_data[,which(colnames(sensitivity_data) %in% samples$sample)] #43 unique cell lines remain, 7 dups
 rm(uhnbreast)
 
-# subset signature scores to samples in dataset
-ubr1_sam <- samples[which(samples$sample %in% colnames(ubr1_sen)),]
-ubr1_sig <- signature_scores[,which(colnames(signature_scores) %in% ubr1_sam$file)]
-
-# compute associations
-ubr1_com <- computeCI(ubr1_sig, ubr1_sen, "uhnbreast1")
-sig_com <- saveSig(sig_com, ubr1_com, "uhnbreast1")
-
-########################
-### UHN Breast PSet2 ###
-########################
-
-# load in UHN Breast PSet
+# UHN Breast2 PSet
 uhnbreast2 <- readRDS("DrugResponse/data/PharmacoSet.RDS")
-
-# get drug sensitivity data
 sensitivity_data <- dcast(uhnbreast2@treatmentResponse$profiles, treatmentid ~ sampleid, value.var = "aac_recomputed")
 rownames(sensitivity_data) <- sensitivity_data$treatmentid
 sensitivity_data <- sensitivity_data[,-c(1)]
@@ -133,6 +129,7 @@ for (i in 1:length(colnames(sensitivity_data))) {
     cell = colnames(sensitivity_data)[i]
     if (cell %in% names(mapping)) {colnames(sensitivity_data)[i] <- unname(mapping[cell])}
 }
+# mapping drug names
 mapping <- c("carboplatin" = "Carboplatinum", "dipyridamole" = "Dipyridamole", 
              "epirubicin" = "Epirubicin", "eribulin" = "Eribulin", "fluvastatin" = "Fluvastatin", 
              "herceptin" = "Herceptin", "lapatinib" = "Lapatinib", "paclitaxel" = "Paclitaxel")
@@ -143,124 +140,124 @@ for (i in 1:length(rownames(sensitivity_data))) {
 ubr2_sen <- sensitivity_data[,which(colnames(sensitivity_data) %in% samples$sample)] #42 unique cell lines remain
 rm(uhnbreast2)
 
-# subset signature scores to samples in dataset
-ubr2_sam <- samples[which(samples$sample %in% colnames(ubr2_sen)),]
-ubr2_sig <- signature_scores[,which(colnames(signature_scores) %in% ubr2_sam$file)]
-
-# compute associations
-ubr2_com <- computeCI(ubr2_sig, ubr2_sen, "uhnbreast2")
-sig_com <- saveSig(sig_com, ubr2_com, "uhnbreast2")
-
-
-#################
-### Gray PSet ###
-#################
-
-# load in Gray PSet
-gray <- readRDS("DrugResponse/data/PSet_GRAY2017.rds")
-gray <- updateObject(gray)
-
-# get drug sensitivity data
+# Gray PSet
+gray <- readRDS("DrugResponse/data/PSet_GRAY2017.rds") |> updateObject()
 sensitivity_data <- as.data.frame(summarizeSensitivityProfiles(gray, sensitivity.measure = "aac_recomputed",  fill.missing = F))
 gray_sen <- sensitivity_data[,which(colnames(sensitivity_data) %in% samples$sample)] #42 unique cell lines remain
 rm(gray)
 
-# subset signature scores to samples in dataset
-gray_sam <- samples[which(samples$sample %in% colnames(gray_sen)),]
-gray_sig <- signature_scores[,which(colnames(signature_scores) %in% gray_sam$file)]
-
-# compute associations
-gray_com <- computeCI(gray_sig, gray_sen, "gray")
-sig_com <- saveSig(sig_com, gray_com, "gray")
-
-
-#################
-### gCSI PSet ###
-#################
-
-# load in gCSI PSet
-gcsi <- readRDS("C:/Users/julia/Desktop/ncRNA Code/data/PSets/gCSI.rds")
-gcsi <- updateObject(gcsi)
-
-# get drug sensitivity data
+# gCSI
+gcsi <- readRDS("C:/Users/julia/Desktop/ncRNA Code/data/PSets/gCSI.rds") |> updateObject()
 sensitivity_data <- as.data.frame(summarizeSensitivityProfiles(gcsi, sensitivity.measure = "aac_recomputed",  fill.missing = F))
 gcsi_sen <- sensitivity_data[,which(colnames(sensitivity_data) %in% samples$sample)] #25 unique cell lines remain, 7 dups
 rm(gcsi)
 
-# subset signature scores to samples in dataset
-gcsi_sam <- samples[which(samples$sample %in% colnames(gcsi_sen)),]
-gcsi_sig <- signature_scores[,which(colnames(signature_scores) %in% gcsi_sam$file)]
-
-# compute associations
-gcsi_com <- computeCI(gcsi_sig, gcsi_sen, "gcsi")
-sig_com <- saveSig(sig_com, gcsi_com, "gcsi")
-
-################# 
-### GDSC PSet ###
-#################
-
-# load in GDSC PSet
-gdsc <- readRDS("C:/Users/julia/Desktop/ncRNA Code/data/PSets/GDSC2-8.2.rds")
-gdsc <- updateObject(gdsc)
-
-# get drug sensitivity data
+# GDSC
+gdsc <- readRDS("C:/Users/julia/Desktop/ncRNA Code/data/PSets/GDSC2-8.2.rds") |> updateObject()
 sensitivity_data <- as.data.frame(summarizeSensitivityProfiles(gdsc, sensitivity.measure = "aac_recomputed",  fill.missing = F))
 gdsc_sen <- sensitivity_data[,which(colnames(sensitivity_data) %in% samples$sample)] #34 unique cell lines remain, 7 dups
 rm(gdsc)
 
-# subset signature scores to samples in dataset
-gdsc_sam <- samples[which(samples$sample %in% colnames(gdsc_sen)),]
-gdsc_sig <- signature_scores[,which(colnames(signature_scores) %in% gdsc_sam$file)]
-
-# compute associations
-gdsc_com <- computeCI(gdsc_sig, gdsc_sen, "gdsc")
-sig_com <- saveSig(sig_com, gdsc_com, "gdsc")
-
-
-#################
-### CTRP PSet ###
-#################
-
-# load in CTRP PSet
+# CTRP
 load("C:/Users/julia/Desktop/ncRNA Code/data/PSets/CTRP.rds")
-
-# get drug sensitivity data
 sensitivity_data <- as.data.frame(summarizeSensitivityProfiles(CTRP, sensitivity.measure = "aac_recomputed",  fill.missing = F))
 ctrp_sen <- sensitivity_data[,which(colnames(sensitivity_data) %in% samples$sample)] #32 unique cell lines remain, 7 dups
 rm(CTRP)
 
-# subset signature scores to samples in dataset
-ctrp_sam <- samples[which(samples$sample %in% colnames(ctrp_sen)),]
-ctrp_sig <- signature_scores[,which(colnames(signature_scores) %in% ctrp_sam$file)]
-
-# compute associations
-ctrp_com <- computeCI(ctrp_sig, ctrp_sen, "ctrp")
-sig_com <- saveSig(sig_com, ctrp_com, "ctrp")
-
-
-#################
-### CCLE PSet ###
-#################
-
-# load in CCLE PSet
-ccle <- readRDS("C:/Users/julia/Desktop/ncRNA Code/data/PSets/CCLE.rds")
-ccle <- updateObject(ccle)
-
-# get drug sensitivity data
+# CCLE
+ccle <- readRDS("C:/Users/julia/Desktop/ncRNA Code/data/PSets/CCLE.rds") |> updateObject()
 sensitivity_data <- as.data.frame(summarizeSensitivityProfiles(ccle, sensitivity.measure = "aac_recomputed",  fill.missing = F))
 ccle_sen <- sensitivity_data[,which(colnames(sensitivity_data) %in% samples$sample)] #23 unique cell lines remain, 7 dups
 rm(ccle)
 
-# subset signature scores to samples in dataset
+
+###########################################################
+# Subset signature associations
+###########################################################
+
+# UHN Breast
+ubr1_sam <- samples[which(samples$sample %in% colnames(ubr1_sen)),]
+ubr1_sig <- signature_scores[,which(colnames(signature_scores) %in% ubr1_sam$file)]
+
+# UHN Breast2
+ubr2_sam <- samples[which(samples$sample %in% colnames(ubr2_sen)),]
+ubr2_sig <- signature_scores[,which(colnames(signature_scores) %in% ubr2_sam$file)]
+
+# GRAY
+gray_sam <- samples[which(samples$sample %in% colnames(gray_sen)),]
+gray_sig <- signature_scores[,which(colnames(signature_scores) %in% gray_sam$file)]
+
+# gCSI
+gcsi_sam <- samples[which(samples$sample %in% colnames(gcsi_sen)),]
+gcsi_sig <- signature_scores[,which(colnames(signature_scores) %in% gcsi_sam$file)]
+
+# GDSC
+gdsc_sam <- samples[which(samples$sample %in% colnames(gdsc_sen)),]
+gdsc_sig <- signature_scores[,which(colnames(signature_scores) %in% gdsc_sam$file)]
+
+# CTRP
+ctrp_sam <- samples[which(samples$sample %in% colnames(ctrp_sen)),]
+ctrp_sig <- signature_scores[,which(colnames(signature_scores) %in% ctrp_sam$file)]
+
+# CCLE
 ccle_sam <- samples[which(samples$sample %in% colnames(ccle_sen)),]
 ccle_sig <- signature_scores[,which(colnames(signature_scores) %in% ccle_sam$file)]
 
-# compute associations
+
+###########################################################
+# Compute concordance index CAS-drug associations
+###########################################################
+
+# UHN Breast
+ubr1_com <- computeCI(ubr1_sig, ubr1_sen, "uhnbreast1")
+sig_com <- saveSig(sig_com, ubr1_com, "uhnbreast1")
+
+# UHN Breast2
+ubr2_com <- computeCI(ubr2_sig, ubr2_sen, "uhnbreast2")
+sig_com <- saveSig(sig_com, ubr2_com, "uhnbreast2")
+
+# GRAY
+gray_com <- computeCI(gray_sig, gray_sen, "gray")
+sig_com <- saveSig(sig_com, gray_com, "gray")
+
+# gCSI
+gcsi_com <- computeCI(gcsi_sig, gcsi_sen, "gcsi")
+sig_com <- saveSig(sig_com, gcsi_com, "gcsi")
+
+# GDSC
+gdsc_com <- computeCI(gdsc_sig, gdsc_sen, "gdsc")
+sig_com <- saveSig(sig_com, gdsc_com, "gdsc")
+
+# CTRP
+ctrp_com <- computeCI(ctrp_sig, ctrp_sen, "ctrp")
+sig_com <- saveSig(sig_com, ctrp_com, "ctrp")
+
+# CCLE
 ccle_com <- computeCI(ccle_sig, ccle_sen, "ccle")
 sig_com <- saveSig(sig_com, ccle_com, "ccle")
 
 
-save(sig_com, ubr1_com, ubr2_com, gray_com, gcsi_com, gdsc_com, ctrp_com, ccle_com, file = "DrugResponse/results/data/indiv_PSet_CI.RData")
+###########################################################
+# Save signature associations
+###########################################################
+
+save(sig_com, ubr1_com, ubr2_com, gray_com, gcsi_com, gdsc_com, ctrp_com, ccle_com, 
+     file = "DrugResponse/results/data/indiv_PSet_CI.RData")
+
+
+###########################################################
+# Define palettes for plotting
+###########################################################
+
+# signature palette
+pal <- c("CAS-Signature1" = "#046C9A", "CAS-2" = "#BBADB9", "CAS-3" = "#7294D4", 
+        "CAS-4" = "#E8E1D9", "CAS-5" = "#AFC5D8", "CAS-6" = "#DF9C93")
+# sensitive vs resistant palette
+pal2 <- c("#899DA4", "#BC4749")
+
+###########################################################
+# Plot waterfall plots
+###########################################################
 
 # TODO: turn this into a function for each pset (if needed)
 png("DrugResponse/results/figures/indiv_PSet_CI/uhnbreast.png", width = 8, height = 5, res = 600, units = "in")
@@ -274,13 +271,11 @@ ggplot(ubr1_com, aes(x = rank, y = ci - 0.5, fill = ifelse(ci > 0.65 | ci < 0.35
 dev.off()
 
 
-####################################
-### All Significant Associations ###
-####################################
+###########################################################
+# Identify Class A Biomarkers
+###########################################################
 
-### CLASS A BIOMARKERS ###
-
-# stricter CI thresholds
+# filter by CI thresholds
 strict_sig_com <- sig_com[which(sig_com$ci > 0.7 | sig_com$ci < 0.3),]
 strict_sig_com <- strict_sig_com[strict_sig_com$FDR < 0.01,]
 
@@ -288,6 +283,7 @@ strict_sig_com <- strict_sig_com[strict_sig_com$FDR < 0.01,]
 strict_sig_com <- strict_sig_com[order(strict_sig_com$ci),]
 strict_sig_com$rank <- 1:nrow(strict_sig_com)
 
+# format factors
 strict_sig_com$rank <- as.factor(strict_sig_com$rank)
 strict_sig_com$drug <- as.factor(strict_sig_com$drug)
 
@@ -295,7 +291,12 @@ strict_sig_com$drug <- as.factor(strict_sig_com$drug)
 strict_sig_com$type <- ifelse(strict_sig_com$ci > 0.5, "Sensitivity", "Resistance")
 strict_sig_com$type <- factor(strict_sig_com$type, levels = c("Sensitivity", "Resistance"))
 
-# plot of all sig associations
+
+###########################################################
+# Plot Class A biomarkers
+###########################################################
+
+# plot vertical
 png("DrugResponse/results/figures/indiv_PSet_CI/all_sig.png", width = 7, height = 15, res = 600, units = "in")
 ggplot(strict_sig_com, aes(x = ci - 0.5, y = rank)) +
     geom_col(aes(fill = signature), color = "black") + scale_x_continuous(limits = c(-0.5, 0.5), labels = function(x) x + 0.5) +
@@ -311,12 +312,12 @@ ggplot(strict_sig_com, aes(x = ci - 0.5, y = rank)) +
     labs(y = "Drug", title = "", x = "Concordance Index (CI)", fill = "Signature") 
 dev.off()
 
-# same graph but horizontal
+# format for same graph but horizontal
 strict_sig_com$rank <- nrow(strict_sig_com):1
 strict_sig_com$rank <- as.factor(strict_sig_com$rank)
 strict_sig_com$drug <- as.factor(strict_sig_com$drug)
  
-
+# plot horizontal
 png("DrugResponse/results/figures/indiv_PSet_CI/ClassA.png", width = 13, height = 6, res = 600, units = "in")
 ggplot(strict_sig_com, aes(x = ci - 0.5, y = rank)) +
     geom_col(aes(fill = signature), color = "black") + scale_x_continuous(limits = c(-0.5, 0.5), labels = function(x) x + 0.5) +
@@ -326,9 +327,16 @@ ggplot(strict_sig_com, aes(x = ci - 0.5, y = rank)) +
 dev.off()
 
 
-# save class A biomarkers
-write.csv(strict_sig_com, file = "DrugResponse/results/data/ClassA_Biomarkers.csv", quote = F, row.names = F)
+###########################################################
+# Save Class A Biomarkers
+###########################################################
 
+write.csv(strict_sig_com, file = "DrugResponse/results/data/ClassA_Biomarkers.csv", 
+          quote = F, row.names = F)
+
+###########################################################
+# Waterfall plot of Class A Biomarkers
+###########################################################
 
 # function to make plot for each signature
 waterfallplot_signature <- function(signature) {
@@ -377,7 +385,11 @@ waterfallplot_signature("Signature6")
 dev.off()
 
 
-### Count per Signature ###
+###########################################################
+# Additional Class A plots
+###########################################################
+
+# count per signature
 png("DrugResponse/results/figures/indiv_PSet_CI/count_per_sig.png", width = 6, height = 4, res = 600, units = "in")
 ggplot(strict_sig_com, aes(x = signature, fill = type)) + 
   geom_bar(stat = "count", size = 0.5, position = "dodge", color = "black", width=0.6) +
@@ -390,8 +402,7 @@ dev.off()
 # table of counts per signature
 #table(strict_sig_com$signature, strict_sig_com$type)
 
-
-### Count per PSet ###
+# count per PSet
 png("DrugResponse/results/figures/indiv_PSet_CI/count_per_pset.png", width = 9, height = 2, res = 600, units = "in")
 ggplot(strict_sig_com, aes(x = "", fill = pset)) + 
   geom_bar(stat = "count", size = 0.5, position = "fill", color = "black", width=0.6) +
