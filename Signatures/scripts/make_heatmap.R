b258f2ff689d0c43052c3c156fb6ed3b6c5ad6a2
julianguyn <julianguyenpt@gmail.com>
2025-02-26 21:34:52 -0500

    refactor: code style

--- Signatures/scripts/make_heatmap.R
+++ Signatures/scripts/make_heatmap.R
@@ -1,33 +1,35 @@
 setwd("C:/Users/julia/Documents/BCaATAC")
 
-library(data.table)
-library(ggplot2)
-library(reshape2)
-library(dplyr)
-library(RColorBrewer)
-library(ggh4x)
-library(ggpubr)
-suppressMessages(library(grid))
-suppressMessages(library(gridExtra))
+# load libraries
+suppressPackageStartupMessages({
+  library(data.table)
+    library(ggplot2)
+    library(reshape2)
+    library(dplyr)
+    library(RColorBrewer)
+    library(ggh4x)
+    library(ggpubr)
+    library(grid)
+    library(gridExtra)
+})
 
 
-# set up palette for plotting
-pal <- c("Signature1" = "#046C9A", "Signature2" = "#BBADB9", "Signature3" = "#7294D4", 
-        "Signature4" = "#E8E1D9", "Signature5" = "#AFC5D8", "Signature6" = "#DF9C93",
-        "Signature7" = "#4E4C67", "Signature8" = "#B4869F", "Signature9" = "#A6B1E1",
-        "Signature10" = "#985F6F")
-#subtype_pal <- c("Basal" = "#394032","Her2" = "#A6A57A", "LumA" = "#8C5E58", "LumB" = "#5A352A", "Normal" = "#8F8073", "Not Available" = "#eFeBF7")
-subtype_pal <- c("Basal" = "#AF4C5B","Her2" = "#EED4D3", "LumA" = "#B3B4D0", "LumB" = "#363E62", "Normal" = "#6365AF", "Not Available" = "#eFeBF7")
-
+###########################################################
+# Load in data
+###########################################################
 
 # read in meta data
 meta <- read.csv("Signatures/data/TCGA_subtype_label.csv")
 
-
 # load in matrix file from NMF
 mat <- read.table("Signatures/results/data/ATAC_heatmap_rank6.png.order.matrix", header = T)
 
-# get signature
+
+###########################################################
+# Format signature data for plotting
+###########################################################
+
+# get signatures
 mat$Signature <- paste0("Signature", 1:6)
 
 # format for plotting
@@ -46,14 +48,28 @@ for (sample in mat$variable) {
     mat[mat$variable == sample,]$signature_assign <- as.character(tmp[which.max(tmp$value),]$Signature)
 }
 
-
 # reorder signatures
 mat$Signature <- paste0(mat$Signature, " ")
 mat$Signature <- factor(mat$Signature, levels = paste0("Signature",6:1," "))
 
 
+###########################################################
+# Set up palettes for heatmaps
+###########################################################
 
-### Plot Heatmap Rank 6 ###
+# signature palette
+pal <- c("Signature1" = "#046C9A", "Signature2" = "#BBADB9", "Signature3" = "#7294D4", 
+        "Signature4" = "#E8E1D9", "Signature5" = "#AFC5D8", "Signature6" = "#DF9C93",
+        "Signature7" = "#4E4C67", "Signature8" = "#B4869F", "Signature9" = "#A6B1E1",
+        "Signature10" = "#985F6F")
+# subtype palette
+subtype_pal <- c("Basal" = "#AF4C5B","Her2" = "#EED4D3", "LumA" = "#B3B4D0", "LumB" = "#363E62", "Normal" = "#6365AF", "Not Available" = "#eFeBF7")
+
+
+###########################################################
+# Plot ATAC-Signature heatmaps
+###########################################################
+
 p1 <- ggplot(mat, aes(x = variable, y = 1, fill = subtype)) + geom_tile(color = NA) +
     theme_void() + scale_fill_manual("BCa\nSubtype", values = subtype_pal,
                     labels = c("Basal", "Her2", "LuminalA", "LuminalB", "Normal", "NA")) +
@@ -86,8 +102,11 @@ grid.arrange(p1, p2, p3, l1, l2, l3, ncol = 9, nrow = 12,
 dev.off()
 
 
+###########################################################
+# Format RNA-Seq NMF matrices for heatmap
+###########################################################
 
-#### plotting for RNA signatures
+# function for processing nmf heatmap files for RNA-Seq
 read_mat <- function(file, rank) {
         
     # load in matrix file from NMF
@@ -117,13 +136,13 @@ read_mat <- function(file, rank) {
     return(mat)
 }
 
-
-
-### Plot Heatmap Rank 6 ###
 mat <- read_mat("Signatures/results/data/RNA_heatmap_rank6.png.order.matrix", 6)
 
 
-### Plot Heatmap Rank 6 ###
+###########################################################
+# Plot RNA-Signature heatmap
+###########################################################
+
 p1 <- ggplot(mat, aes(x = variable, y = 1, fill = subtype)) + geom_tile(color = NA) +
     theme_void() + scale_fill_manual("BCa\nSubtype", values = subtype_pal,
                     labels = c("Basal", "Her2", "LuminalA", "LuminalB", "Normal", "NA")) +
@@ -153,35 +172,4 @@ grid.arrange(p1, p2, p3, l1, l2, l3, ncol = 9, nrow = 12,
                           c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,6,NA),
                           c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,6,NA),
                           c(3,3,3,3,3,3,3,6,NA),c(3,3,3,3,3,3,3,NA,NA)))
-dev.off()
-
-
-
-p1 <- make_p1(mat6)
-p2 <- make_p2(mat6)
-p3 <- make_p3(mat6)
-
-# extract legends
-l1 <- as_ggplot(get_legend(p1))
-l2 <- as_ggplot(get_legend(p2))
-l3 <- as_ggplot(get_legend(p3))
-p1 <- p1+theme(legend.position = "none")
-p2 <- p2+theme(legend.position = "none")
-p3 <- p3+theme(legend.position = "none")
-
-png("Signatures/results/figures/RNA_rank6_heatmap.png", width = 10, height = 4, res = 600, units = "in")
-grid.arrange(p1, p2, p3, l1, l2, l3, ncol = 9, nrow = 13,
-    layout_matrix = rbind(c(1,1,1,1,1,1,1,4,5),
-                        c(2,2,2,2,2,2,2,4,5),
-                        c(3,3,3,3,3,3,3,4,5),
-                        c(3,3,3,3,3,3,3,4,5),
-                        c(3,3,3,3,3,3,3,4,5),
-                        c(3,3,3,3,3,3,3,4,5),
-                        c(3,3,3,3,3,3,3,4,5),
-                        c(3,3,3,3,3,3,3,6,NA),
-                        c(3,3,3,3,3,3,3,6,NA),
-                        c(3,3,3,3,3,3,3,6,NA),
-                        c(3,3,3,3,3,3,3,6,NA),
-                        c(3,3,3,3,3,3,3,6,NA),
-                        c(3,3,3,3,3,3,3,NA,NA)))
 dev.off()
