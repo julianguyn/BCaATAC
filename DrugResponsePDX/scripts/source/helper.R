f9287c2eb6ed1a80e188c79afcbc9aa680c9e6fc
julianguyn <julianguyenpt@gmail.com>
2025-07-28 23:21:14 -0400

    feat: complete PDX validation plots

--- DrugResponsePDX/scripts/source/helper.R
+++ DrugResponsePDX/scripts/source/helper.R
@@ -19,7 +19,7 @@ mapping <- c("X108099P1" = "108099",
              "REFB047" = "REF047",
              "REF047_S26_L002" = "REF047",
              "REFS036P1A" = "REF036",
-             "REF_S_038" = "REFS038"
+             "REF_S_038" = "REF038"
 )
 
 map_pdx <- function(models) {
@@ -55,7 +55,7 @@ get_ARCHE <- function(df) {
 
 #' Plot waterfall plot of ARCHE score coloured by mRECIST 
 #'
-#' Append columns to treatment response matrices with PDX ARCHE signature scores.
+#' Creates waterfall plot of PDX ARCHE scores coloured by mRECIST
 #' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
 #' @param arche string. ARCHE name, one of ARCHE[1:6] 
 #' @param df string. Drug name (should match entry under 'drug' column of df)
@@ -90,4 +90,286 @@ waterfall_mRECIST <- function(df, arche, drug, plot.indiv = F) {
         dev.off()
     }
     return(p)
+}
+
+
+#' Plot average ARCHE score for each mRECIST 
+#'
+#' Creates bar plot of average ARCHE score for each mRECIST
+#' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
+#' @param arche string. ARCHE name, one of ARCHE[1:6] 
+#' @param df string. Drug name (should match entry under 'drug' column of df)
+#' @param plot.indiv boolean. TRUE if waterfall plot should be saved as an individual file.
+#' @return ggplot object of bar plot.
+#' 
+avg_ARCHE_mRECIST <- function(df, arche, drug, plot.indiv = F) {
+
+    # compute average ARCHE score for each mRECIST category
+    df <- df %>%
+        group_by(mRECIST) %>%
+        summarize(
+            mean_score = mean(.data[[arche]], na.rm = TRUE),
+            sd_score = sd(.data[[arche]], na.rm = TRUE),
+            .groups = "drop"
+        )
+
+    # obtain min and max values
+    y <- max(abs(min(df$mean_score)), max(df$mean_score)) |> ceiling()
+    
+    # create plot
+    p <- ggplot(df, aes(x = mRECIST, y = mean_score)) +
+        geom_bar(stat = "identity", color = "black", aes(fill = mRECIST)) + ylim(c(-y, y)) +
+        geom_hline(yintercept = 0) +
+        geom_errorbar(aes(ymin = mean_score - sd_score, ymax = mean_score + sd_score), width = 0.2) +
+        scale_fill_manual(values = c("#136F63", "#9DCBBA", "#FFADA1", "#B02E0C"),
+                            labels = c('CR' = 'Complete\nResponse', 
+                                    'PD' = 'Progressive\nDisease', 
+                                    'SD' = 'Stable\nDisease',
+                                    'PR' = 'Partial\nResponse')) +
+        scale_x_discrete(labels = c('CR' = 'Complete\nResponse', 
+                                    'PD' = 'Progressive\nDisease', 
+                                    'SD' = 'Stable\nDisease',
+                                    'PR' = 'Partial\nResponse')) +
+        labs(x = "\nmRECIST", y = paste(arche, "Score")) +
+        theme_classic() + 
+        theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5), legend.position = "none")
+
+    # print out plot if needed
+    if (plot.indiv == TRUE) {
+        path = paste0("DrugResponsePDX/results/figures/indiv_plots/mRECIST_avg_", arche, "_", drug, ".png")
+        png(path, width = 4, height = 5, res = 600, units = "in")
+        print(p)
+        dev.off()
+    }
+    return(p)
+
+}
+
+#' Plot waterfall plot of ARCHE score coloured by ARCHE predictive accuracy (compared to mRECIST outcome) 
+#'
+#' Creates waterfall plot of PDX ARCHE scores coloured by predictive accuracy
+#' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
+#' @param arche string. ARCHE name, one of ARCHE[1:6] 
+#' @param df string. Drug name (should match entry under 'drug' column of df)
+#' @param plot.indiv boolean. TRUE if waterfall plot should be saved as an individual file.
+#' @return ggplot object of waterfall plot.
+#' 
+waterfall_mRECIST_accuracy <- function(df, arche, drug, plot.indiv = F) {
+
+    # create ranking order
+    df <- df[order(df[[arche]], decreasing = T),]
+    df$rank <- 1:nrow(df)
+
+    # binarize response
+    df$predicted <- ifelse(df[[arche]] >= 0, 1, 0)
+    df$outcome <- ifelse(df$mRECIST %in% c('CR', 'PR'), 1, 0)
+
+    # determine incorrect predictions
+    df$accuracy <- ifelse(df$predicted == df$outcome, 'correct', 'incorrect')
+
+    # obtain min and max values
+    y <- max(abs(min(df[[arche]])), max(df[[arche]])) |> ceiling()
+
+    # create plot
+    p <- ggplot(df, aes(x = rank, y = .data[[arche]], fill = accuracy)) + 
+        geom_bar(stat = "identity", color = "black") + geom_hline(yintercept = 0) +
+        scale_fill_manual(values = c("#1E4076", "#A94745")) +
+        theme_classic() + theme(legend.key.size = unit(0.8, 'cm'), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+        ylim(c(-y, y)) + labs(x = "PDX Model", y = paste(arche, "Score"), fill = "ARCHE Score\nAccuracy") 
+    
+    # print out plot if needed
+    if (plot.indiv == TRUE) {
+        path = paste0("DrugResponsePDX/results/figures/indiv_plots/mRECIST_waterfall_accuracy", arche, "_", drug, ".png")
+        png(path, width=175, height=125, units='mm', res = 600, pointsize=80)
+        print(p)
+        dev.off()
+    }
+    return(p)
+}
+
+
+#' Plot ROC of ARCHE predicted outcome compared to mRECIST 
+#'
+#' Creates ROC of ARCHE predictive accuracy compared to mRECIST (binarized)
+#' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
+#' @param arche string. ARCHE name, one of ARCHE[1:6] 
+#' @param df string. Drug name (should match entry under 'drug' column of df)
+#' @param plot.indiv boolean. TRUE if waterfall plot should be saved as an individual file.
+#' @return ROC plot.
+#' 
+ROC_mRECIST <- function(df, arche, drug, plot.indiv = F) {
+
+    # binarize ARCHE prediction and mRECIST outcome
+    predicted <- ifelse(df[[arche]] >= 0, 1, 0)
+    actual <- ifelse(df$mRECIST %in% c('CR', 'PR'), 1, 0)
+
+    if (length(unique(predicted)) > 1 && length(unique(actual)) > 1) {
+        # compute ARCHE performance
+        pred <- prediction(predicted, actual)
+        perf <- performance(pred, "tpr", "fpr")
+
+        # create df to plot performance
+        toPlot <- data.frame(
+            fpr = perf@x.values[[1]],
+            tpr = perf@y.values[[1]]
+        )
+
+        # create ROC
+        p <- ggplot(toPlot, aes(x = fpr, y = tpr)) +
+            geom_line(color = "darkgreen", size = 1.2) +
+            geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "grey") +
+            labs(
+                x = "False Positive Rate",
+                y = "True Positive Rate"
+            ) +
+            theme_classic() +
+            theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5))
+
+        # print out plot if needed
+        if (plot.indiv == TRUE) {
+            path = paste0("DrugResponsePDX/results/figures/indiv_plots/mRECIST_waterfall_accuracy", arche, "_", drug, ".png")
+            png(path, width=125, height=125, units='mm', res = 600, pointsize=80)
+            print(p)
+            dev.off()
+        }
+        return(p)
+    } else {
+        return(ggplot())
+    }
+    
+}
+
+
+#' Create panel of plots based on ARCHE association with mRECIST
+#'
+#' Uses pre-defined functions (above) to create various ARCHE plots based on mRECIST 
+#' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
+#' @param df string. Drug name (should match entry under 'drug' column of df)
+#' @export Panel of four plots.
+#' 
+assess_ARCHE_mRECIST <- function(df, drug) {
+
+    # subset for variables of interest
+    df <- df[df$drug == drug,]
+    if (c('slope') %in% colnames(df)) {
+        df <- df[,-which(colnames(df) %in% c('slope', 'AUC'))]
+    }
+    df$mRECIST <- factor(df$mRECIST, levels = c("CR", "PR", "SD", "PD"))
+    df <- df[complete.cases(df),]
+
+    # iterate through each ARCHE
+    for (arche in paste0('ARCHE', 1:6)) {
+        p1 <- waterfall_mRECIST(df, arche, drug)
+        p2 <- avg_ARCHE_mRECIST(df, arche, drug)
+        p3 <- waterfall_mRECIST_accuracy(df, arche, drug)
+        p4 <- ROC_mRECIST(df, arche, drug)
+
+        # create figure panel
+        path = paste0("DrugResponsePDX/results/figures/mRECIST/", arche, "_", drug, ".png")
+        png(path, width=8, height=6, units='in', res = 600, pointsize=80)
+        print(ggarrange(p1, p2, p3, p4, ncol = 2, nrow = 2))
+        dev.off()
+    }
+}
+
+#' Plot waterfall plot of AUC coloured by ARCHE score
+#'
+#' Creates waterfall plot of PDX AUC response coloured by ARCHE scores 
+#' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
+#' @param arche string. ARCHE name, one of ARCHE[1:6] 
+#' @param df string. Drug name (should match entry under 'drug' column of df)
+#' @param plot.indiv boolean. TRUE if waterfall plot should be saved as an individual file.
+#' @return ggplot object of waterfall plot.
+#' 
+waterfall_AUC <- function(df, arche, drug, plot.indiv = F) {
+
+    # order samples
+    df$AUC <- as.numeric(df$AUC)
+    df <- df[order(df$AUC, decreasing = T),]
+    df$rank <- 1:nrow(df)
+
+    # obtain min and max values
+    score <- max(abs(min(df[[arche]])), max(df[[arche]])) |> ceiling()
+
+    # create plot
+    p <- ggplot(df, aes(x = rank, y = AUC, fill = .data[[arche]])) + 
+        geom_bar(stat = "identity", color = "black") + 
+        geom_hline(yintercept = 0) +
+        scale_fill_gradientn(colors = c("#BC4749", "#F8F1F8", "#077293"), limits = c(-score, score)) +
+        theme_classic() + 
+        theme(legend.key.size = unit(0.8, 'cm'), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+        labs(x = "PDX Model", y = "AUC", fill = paste(arche, "\nScore")) 
+
+    # print out waterfall plot if needed
+    if (plot.indiv == TRUE) {
+        path = paste0("DrugResponsePDX/results/figures/indiv_plots/AUC_waterfall_", arche, "_", drug, ".png")
+        png(path, width=175, height=125, units='mm', res = 600, pointsize=80)
+        print(p)
+        dev.off()
+    }
+    return(p)
+}
+
+#' Plot scatter plot of AUC vs ARCHE score
+#'
+#' Creates scatter plot of PDX AUC response vs ARCHE scores 
+#' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
+#' @param arche string. ARCHE name, one of ARCHE[1:6] 
+#' @param df string. Drug name (should match entry under 'drug' column of df)
+#' @param plot.indiv boolean. TRUE if scatter plot should be saved as an individual file.
+#' @return ggplot object of scatter plot.
+#' 
+scatter_AUC <- function(df, arche, drug, plot.indiv = F) {
+
+    # compute pearson's correlation
+    pc <- cor.test(df[[arche]], df$AUC, method = "pearson", alternative = "two.sided")
+
+    # create plot
+    p <- ggplot(df, aes(x = .data[[arche]], y = AUC)) +
+        geom_point(size = 2) +
+        geom_smooth(method='lm', formula= y~x) +
+        theme_classic() +
+        theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5)) +
+        labs(x = paste(arche, "Score")) +
+        ggtitle(paste("PCC:", round(pc$estimate, 4), ", pval:", round(pc$p.value, 4)))
+
+    # print out plot if needed
+    if (plot.indiv == TRUE) {
+        path = paste0("DrugResponsePDX/results/figures/indiv_plots/AUC_scatter", arche, "_", drug, ".png")
+        png(path, width=125, height=125, units='mm', res = 600, pointsize=80)
+        print(p)
+        dev.off()
+    }
+    return(p)
+}
+
+
+#' Create panel of plots based on ARCHE association with AUC
+#'
+#' Uses pre-defined functions (above) to create various ARCHE plots based on AUC 
+#' @param df dataframe. Treatment response dataframe with ARCHE scores concatenated
+#' @param df string. Drug name (should match entry under 'drug' column of df)
+#' @export Panel of four plots.
+#' 
+assess_ARCHE_AUC <- function(df, drug) {
+
+    # subset for variables of interest
+    df <- df[df$drug == drug,]
+    if (c('slope') %in% colnames(df)) {
+        df <- df[,-which(colnames(df) %in% c('slope', 'mRECIST'))]
+    }
+    df$AUC <- as.numeric(df$AUC)
+    df <- df[complete.cases(df),]
+
+    # iterate through each ARCHE
+    for (arche in paste0('ARCHE', 1:6)) {
+        p1 <- waterfall_AUC(df, arche, drug)
+        p2 <- scatter_AUC(df, arche, drug)
+
+        # create figure panel
+        path = paste0("DrugResponsePDX/results/figures/AUC/", arche, "_", drug, ".png")
+        png(path, width=5, height=6, units='in', res = 600, pointsize=80)
+        print(ggarrange(p1, p2, ncol = 1, nrow = 2))
+        dev.off()
+    }
 }
--- DrugResponsePDX/scripts/validate_biomarkers.R
+++ DrugResponsePDX/scripts/validate_biomarkers.R
@@ -6,6 +6,8 @@ suppressPackageStartupMessages({
     library(ggplot2)
     library(RColorBrewer)
     library(dplyr)
+    library(ROCR)
+    library(ggpubr)
 })
 
 source("DrugResponsePDX/scripts/source/helper.R")
@@ -46,195 +48,43 @@ new_xeva_mRECIST <- get_ARCHE(new_xeva_mRECIST)
 
 
 ###########################################################
-# Assess ARCHE treatment response associations 
+# Assess ARCHE drug response associations (via mRECIST)
 ###########################################################
 
-# using sourced 
-assess_ARCHE_mRECIST <- function(df, drug) {
-    # subset for variables of interest
-    df <- df[df$drug == drug,]
-    if (c('slope') %in% colnames(df)) {
-        df[,-which(colnames(df) %in% c('slope', 'AUC'))]
-    }
-    df <- df[complete.cases(df),]
-    df$mRECIST <- factor(df$mRECIST, levels = c("CR", "PR", "SD", "PD"))
+# using sourced assess_ARCHE_mRECIST()
+assess_ARCHE_mRECIST(old_response, 'AZD-5305')
+assess_ARCHE_mRECIST(old_response, 'AZD-8205')
+assess_ARCHE_mRECIST(old_response, 'BMN-673')
+assess_ARCHE_mRECIST(old_response, 'CARBOPLATIN')
+assess_ARCHE_mRECIST(old_response, 'CFI-400945')
+assess_ARCHE_mRECIST(old_response, 'CFI-402257')
+assess_ARCHE_mRECIST(old_response, 'ERIBULIN')
+assess_ARCHE_mRECIST(old_response, 'TAXOL')
 
-    # iterate through each ARCHE
-    for (arche in paste0('ARCHE', 1:6)) {
-        p1 <- waterfall_mRECIST(df, arche, drug)
-    }
-}
-
-
-
-# df <- old_response[old_response$drug == "ERIBULIN",]
-
+assess_ARCHE_mRECIST(new_xeva_mRECIST, 'AZD-5305')
+assess_ARCHE_mRECIST(new_xeva_mRECIST, 'AZD-8205')
+assess_ARCHE_mRECIST(new_xeva_mRECIST, 'CDX-011')
+assess_ARCHE_mRECIST(new_xeva_mRECIST, 'CFI-400945')
+assess_ARCHE_mRECIST(new_xeva_mRECIST, 'CX-5461')
+assess_ARCHE_mRECIST(new_xeva_mRECIST, 'FLUVASTATIN')
 
 ###########################################################
-# Plot waterfall by AUC
+# Assess ARCHE drug response associations (via AUC)
 ###########################################################
 
-# order samples
-pac$AUC <- as.numeric(pac$AUC)
-pac_auc <- pac[order(pac$AUC, decreasing = T),]
-pac_auc$rank <- 1:nrow(pac_auc)
+# using sourced assess_ARCHE_AUC()
+assess_ARCHE_AUC(old_response, 'AZD-5305')
+assess_ARCHE_AUC(old_response, 'AZD-8205')
+assess_ARCHE_AUC(old_response, 'BMN-673')
+assess_ARCHE_AUC(old_response, 'CARBOPLATIN')
+assess_ARCHE_AUC(old_response, 'CFI-400945')
+assess_ARCHE_AUC(old_response, 'CFI-402257') 
+assess_ARCHE_AUC(old_response, 'ERIBULIN')
+assess_ARCHE_AUC(old_response, 'TAXOL')
 
-# plot waterfall plot coloured by signature score
-png("DrugResponsePDX/results/figures/paclitaxel_pdx_sig_waterfall.png", width=175, height=125, units='mm', res = 600, pointsize=80)
-ggplot(pac_auc, aes(x = rank, y = AUC, fill = sig5)) + 
-    geom_bar(stat = "identity", color = "black") + geom_hline(yintercept = 0) +
-    scale_fill_gradientn(colors = c("#BC4749", "#F8F1F8", "#077293"), limits = c(-45, 45)) +
-    theme_classic() + theme(legend.key.size = unit(0.8, 'cm'), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-    labs(x = "PDX Model", y = "AUC", fill = "Signature 5\nScore") 
-dev.off()
-
-
-###########################################################
-# Plot waterfall by slope
-###########################################################
-
-# order samples
-pac$slope <- as.numeric(pac$slope)
-pac_slp <- pac[order(pac$slope, decreasing = T),]
-pac_slp$rank <- 1:nrow(pac_slp)
-
-# plot waterfall plot coloured by signature score
-png("DrugResponsePDX/results/figures/paclitaxel_pdx_slope_waterfall.png", width=175, height=125, units='mm', res = 600, pointsize=80)
-ggplot(pac_slp, aes(x = rank, y = slope, fill = sig5)) + 
-    geom_bar(stat = "identity", color = "black") + geom_hline(yintercept = 0) +
-    scale_fill_gradientn(colors = c("#BC4749", "#F8F1F8", "#077293"), limits = c(-45, 45)) +
-    theme_classic() + theme(legend.key.size = unit(0.8, 'cm'), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-    labs(x = "PDX Model", y = "Slope", fill = "Signature 5\nScore") 
-dev.off()
-
-
-###########################################################
-# Plot waterfall of average signature score per mRECIST
-###########################################################
-
-# average signature 5 score for each mRECIST category
-pac_mr <- pac_mr %>%
-  group_by(mRECIST) %>%
-  summarize(mean_score = mean(sig5), sd_score = sd(sig5))
-
-
-png("DrugResponsePDX/results/figures/paclitaxel_pdx_mrecist.png", width = 4, height = 5, res = 600, units = "in")
-ggplot(pac_mr, aes(x = mRECIST, y = mean_score)) +
-  geom_bar(stat = "identity", color = "black", aes(fill = mRECIST)) + ylim(c(-50, 50)) +
-  geom_hline(yintercept = 0) +
-  geom_errorbar(aes(ymin = mean_score - sd_score, ymax = mean_score + sd_score), width = 0.2) +
-  scale_fill_manual(values = c("#136F63", "#9DCBBA", "#FFADA1", "#B02E0C"),
-                      labels = c('CR' = 'Complete\nResponse', 
-                              'PD' = 'Progressive\nDisease', 
-                              'SD' = 'Stable\nDisease',
-                              'PR' = 'Partial\nResponse')) +
-  scale_x_discrete(labels = c('CR' = 'Complete\nResponse', 
-                              'PD' = 'Progressive\nDisease', 
-                              'SD' = 'Stable\nDisease',
-                              'PR' = 'Partial\nResponse')) +
-  labs(x = "\nmRECIST", y = "Signature 5 Similarity Score") +
-  theme_classic() + theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5), legend.position = "none")
-dev.off()
-
-
-###########################################################
-# Plot waterfall of prediction accuracy
-###########################################################
-
-# keep only AUC
-response <- response[,colnames(response) %in% c("patient.id", "AUC", "drug")]
-#dcast(response, drug ~ patient.id, value.var = "AUC") cant' do this bc not all samples have all drugs
-
-
-# read in signature scores
-scores <- as.data.frame(t(read.table("DrugResponsePDX/data/chromvar/bca_sign.Zscore.txt")))
-colnames(scores) <- paste0("Signature", 1:6)
-# sample colname
-
-
-# read in preclinical biomarkers
-classA <- read.csv("DrugResponse/results/data/ClassA_Biomarkers.csv")
-classB <- read.csv("DrugResponse/results/data/ClassB_Biomarkers.csv")
-classC <- read.csv("DrugResponse/results/data/ClassC_Biomarkers.csv")
-
-# get all drugs with predictions
-all_drugs <- unique(c(classA$drug, classB$drug, classC$drug))
-
-# TODO: remove "pitstop2"
-
-# subset for just paclitaxel
-pac <- response[response$drug == "TAXOL",]
-pac <- pac[-which(is.na(pac$patient.id)),]
-pac$AUC <- as.numeric(pac$AUC)
-pac$sig5 <- NA
-
-# get signature scores
-for (i in 1:nrow(pac)) {
-    sample = pac$patient.id[i]
-    pac$sig5[i] <- scores[gsub("_", "", gsub("X", "", rownames(scores))) == sample,]$Signature5
-}
-
-pac$quadrant <- ifelse(pac$AUC > 0, ifelse(pac$sig5 > 0, "Q1", "Q2"), ifelse(pac$sig5 < 0.5, "Q3", "Q4"))
-
-# plot 
-x = max(abs(min(pac$sig5)), max(pac$sig5))
-y = max(abs(min(pac$AUC)), max(pac$AUC))
-
-pac <- pac[order(pac$AUC, decreasing = T),]
-pac$rank <- 1:nrow(pac)
-pac$rank <- as.factor(pac$rank)
-
-pac$pred <- ifelse(pac$quadrant %in% c("Q2", "Q3"), "Accurate", "Inaccurate") 
-pac$pred <- factor(pac$pred, levels = c("Accurate", "Inaccurate"))
-
-png("DrugResponsePDX/results/figures/paclitaxel_pdx_waterfall.png", width = 8, height = 5, res = 600, units = "in")
-ggplot(pac, aes(x = AUC, y = rank)) +
-    geom_col(aes(fill = pred), color = "black") + scale_x_continuous(limits = c(-y, y), labels = function(x) x + 0.5) +
-    #scale_y_discrete(breaks = strict_sig_com$rank, labels = strict_sig_com$drug) +
-    scale_fill_manual(values = c("#899DA4", "#BC4749")) + 
-    theme_classic() + theme(axis.text.x = element_blank()) + geom_vline(xintercept = 0) +
-    labs(y = "PDX Model", title = "", x = "True AUC", fill = "Signature Prediction") + coord_flip()
-dev.off()
-
-
-###########################################################
-# Plot accuracy quadrants
-###########################################################
-
-png("DrugResponsePDX/results/figures/paclitaxel_pdx.png", width=160, height=125, units='mm', res = 600, pointsize=80)
-ggplot(pac, aes(x = sig5, y = AUC, fill = patient.id)) + 
-    geom_rect(fill = "#EDFFEF", color = NA, xmin = 0, xmax = Inf, ymin = -Inf, ymax = 0) +
-    geom_rect(fill = "#EDFFEF", color = NA, xmin = -Inf, xmax = 0, ymin = 0, ymax = Inf) + 
-    geom_rect(fill = "#FFECEC", color = "black", linetype = "dotted", xmin = -Inf, xmax = 0, ymin = -Inf, ymax = 0) + 
-    geom_rect(fill = "#FFECEC", color = "black", linetype = "dotted", xmin = 0, xmax = Inf, ymin = 0, ymax = Inf) + 
-    geom_point(size = 4, shape = 21) + scale_fill_brewer(palette = "Set3") +
-    theme_classic() + theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5),
-                            plot.title = element_text(hjust = 0.5, size = 16), legend.key.size = unit(0.7, 'cm')) +
-    xlim(-x, x) + ylim(-y, y) + labs(x = "Signature 5 Similarity Score", fill = "PDX Model", title = "Paclitaxel") 
-dev.off()
-
-
-# subset for just UNC0642
-unc <- response[response$drug == "UNC0642",]
-unc <- unc[-which(is.na(unc$patient.id)),]
-unc$AUC <- as.numeric(unc$AUC)
-unc$sig4 <- NA
-
-# get signature scores
-for (i in 1:nrow(unc)) { unc$sig4[i] <- scores[gsub("_", "", gsub("X", "", rownames(scores))) == unc$patient.id[i],]$Signature4 }
-
-# plot 
-x = max(abs(min(unc$sig4)), max(unc$sig4))
-y = max(abs(min(unc$AUC)), max(unc$AUC))
-
-png("DrugResponsePDX/results/figures/unc0642_pdx.png", width=160, height=125, units='mm', res = 600, pointsize=80)
-ggplot(unc, aes(x = sig4, y = AUC, fill = patient.id)) + 
-    geom_rect(fill = "#EDFFEF", color = NA, xmin = 0, xmax = Inf, ymin = -Inf, ymax = 0) +
-    geom_rect(fill = "#EDFFEF", color = NA, xmin = -Inf, xmax = 0, ymin = 0, ymax = Inf) + 
-    geom_rect(fill = "#FFECEC", color = "black", linetype = "dotted", xmin = -Inf, xmax = 0, ymin = -Inf, ymax = 0) + 
-    geom_rect(fill = "#FFECEC", color = "black", linetype = "dotted", xmin = 0, xmax = Inf, ymin = 0, ymax = Inf) + 
-    geom_point(size = 4, shape = 21) + scale_fill_brewer(palette = "Set3") +
-    theme_classic() + theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5),
-                            plot.title = element_text(hjust = 0.5, size = 16), legend.key.size = unit(0.7, 'cm')) +
-    xlim(-x, x) + ylim(-y, y) + labs(x = "Signature 4 Similarity Score", fill = "PDX Model", title = "UNC0642") 
-dev.off()
+assess_ARCHE_AUC(new_xeva_auc, 'AZD-5305')
+assess_ARCHE_AUC(new_xeva_auc, 'AZD-8205')
+assess_ARCHE_AUC(new_xeva_auc, 'CDX-011')
+assess_ARCHE_AUC(new_xeva_auc, 'CFI-400945')
+assess_ARCHE_AUC(new_xeva_auc, 'CX-5461')
+assess_ARCHE_AUC(new_xeva_auc, 'FLUVASTATIN')
