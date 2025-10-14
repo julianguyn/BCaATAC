aa20f293740827027a6960a9082157e1219d57c9
Julia Nguyen <julianguyenpt@gmail.com>
2025-10-13 21:31:27 -0400

    feat: assess BAR

--- DrugResponsePDX/scripts/validate_biomarkers.R
+++ DrugResponsePDX/scripts/validate_biomarkers.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/julia/Documents/BCaATAC")
+setwd("/Users/julianguyen/Documents/BCaATAC")
 
 # load libraries
 suppressPackageStartupMessages({
@@ -10,7 +10,7 @@ suppressPackageStartupMessages({
     library(ggpubr)
 })
 
-source("DrugResponsePDX/scripts/source/helper.R")
+source("source/DrugResponsePDX/helper.R")
 
 ###########################################################
 # Load in data
@@ -20,6 +20,8 @@ source("DrugResponsePDX/scripts/source/helper.R")
 old_response <- as.data.frame(read_excel("DrugResponsePDX/data/drugresponse/DrugResponse_PDX.xlsx", sheet = 1))
 new_xeva_auc <- read.csv("DrugResponsePDX/data/Jul232025-Xeva/auc_reps.csv")
 new_xeva_mRECIST <- read.csv("DrugResponsePDX/data/Jul232025-Xeva/mRECIST_reps.csv")
+bar <- as.data.frame(read_excel("DrugResponsePDX/data/Jul232025-Xeva/PDX_BR_BAR_grouped_Julia_8205_TAX_selectedmodels.xlsx", sheet = 1))
+bar$patient.id <- bar$PDX_ID
 
 # read in signature scores
 scores <- as.data.frame(t(read.table("DrugResponsePDX/data/chromvar/bca_sign.Zscore.txt")))
@@ -35,6 +37,7 @@ rownames(scores) <- map_pdx(rownames(scores))
 old_response$patient.id <- map_pdx(old_response$patient.id)
 new_xeva_auc$patient.id <- map_pdx(new_xeva_auc$patient.id)
 new_xeva_mRECIST$patient.id <- map_pdx(new_xeva_mRECIST$patient.id)
+bar$patient.id <- map_pdx(bar$patient.id)
 
 
 ###########################################################
@@ -45,7 +48,7 @@ new_xeva_mRECIST$patient.id <- map_pdx(new_xeva_mRECIST$patient.id)
 old_response <- get_ARCHE(old_response)
 new_xeva_auc <- get_ARCHE(new_xeva_auc)
 new_xeva_mRECIST <- get_ARCHE(new_xeva_mRECIST)
-
+bar <- get_ARCHE(bar)
 
 ###########################################################
 # Assess ARCHE drug response associations (via mRECIST)
@@ -72,7 +75,6 @@ assess_ARCHE_mRECIST(new_xeva_mRECIST, 'FLUVASTATIN')
 # Assess ARCHE drug response associations (via AUC)
 ###########################################################
 
-# using sourced assess_ARCHE_AUC()
 assess_ARCHE_AUC(old_response, 'AZD-5305')
 assess_ARCHE_AUC(old_response, 'AZD-8205')
 assess_ARCHE_AUC(old_response, 'BMN-673')
@@ -88,3 +90,10 @@ assess_ARCHE_AUC(new_xeva_auc, 'CDX-011')
 assess_ARCHE_AUC(new_xeva_auc, 'CFI-400945')
 assess_ARCHE_AUC(new_xeva_auc, 'CX-5461')
 assess_ARCHE_AUC(new_xeva_auc, 'FLUVASTATIN')
+
+###########################################################
+# Assess ARCHE drug response associations (via BAR)
+###########################################################
+
+assess_ARCHE_BAR(bar, 'AZD-8205')
+assess_ARCHE_BAR(bar, 'PACLITAXEL')
