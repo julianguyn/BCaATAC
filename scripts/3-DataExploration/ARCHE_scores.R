0a48207ae7706f60f689dce09045a65d77860e2f
Julia Nguyen <julianguyenpt@gmail.com>
2025-11-24 10:54:15 -0500

    feat: compare PDX scoring on 50k sites

--- scripts/3-DataExploration/ARCHE_scores.R
+++ scripts/3-DataExploration/ARCHE_scores.R
@@ -18,8 +18,12 @@ source("utils/plots/ARCHE_scores_heatmap.R")
 # read in scoring on 20k sites
 p2 <- read.table("data/rawdata/pdx/chromvar-Nov132025-20k/BCa_PDXs.Zscore.txt")
 colnames(p2) <- sub("_.*", "", sub("X", "", colnames(p2)))
-p2 <- p2[,-which(colnames(p2) %in% "104987")]
+p2 <- p2[,-which(colnames(p2) %in% "104987")]       # all NA
 
+# read in scoring on 20k sites
+p5 <- read.table("data/rawdata/pdx/chromvar-Nov242025-50k/PDXs_50k.Zscore.txt")
+colnames(p5) <- sub("_.*", "", sub("X", "", colnames(p5)))
+p5 <- p5[,-which(colnames(p5) %in% "104987")]       # A3-6 NA
 
 # read in scoring on all sites (old)
 pa <- read.table("data/rawdata/pdx/chromvar/bca_sign.Zscore.txt")
@@ -27,8 +31,8 @@ colnames(pa) <- sub("X", "", colnames(pa))
 rownames(pa) <- paste0("ARCHE", 1:6)
 
 # read in scoring on all sites (new)
-p3 <- read.table("data/rawdata/pdx/chromvar-Nov132025-all/BCa_PDXs.Zscore.txt")
-colnames(p3) <- sub("_.*", "", sub("X", "", colnames(p3)))
+pn <- read.table("data/rawdata/pdx/chromvar-Nov132025-all/BCa_PDXs.Zscore.txt")
+colnames(pn) <- sub("_.*", "", sub("X", "", colnames(pn)))
 # note: 104987 has scores here
 
 # read in scoring on cell lines (old)
@@ -42,12 +46,13 @@ c_subtypes <- get_cell_subtype()
 ###########################################################
 
 plot_ARCHE_scores_heatmap(p2, "PDX_20k")
+plot_ARCHE_scores_heatmap(p5, "PDX_50k")
 plot_ARCHE_scores_heatmap(pa, "PDX_all_old")
 plot_ARCHE_scores_heatmap(p3, "PDX_all_new")
 plot_ARCHE_scores_heatmap(c1, "CCLs_all_old", c_subtypes)
 
 ###########################################################
-# Compare 20k to all ARCHE sites
+# Compare ARCHE signature subsets
 ###########################################################
 
-plot_ARCHE_scores_compare(p2, p3, label = "20k sites", "PDX")
+plot_ARCHE_scores_compare(p2, p5, pn, "PDX")
--- utils/plots/ARCHE_scores_heatmap.R
+++ utils/plots/ARCHE_scores_heatmap.R
@@ -18,45 +18,51 @@ plot_ARCHE_scores_heatmap <- function(df, label, meta) {
     df <- znorm(df)
 
     # set colours for plotting
-    score_pal = colorRamp2(seq(min(norm_df), max(norm_df), length = 3), c("#C3BFCC", "#F8F1F8", "#077293"))
+    score_pal = colorRamp2(seq(min(df), max(df), length = 3), c("#C3BFCC", "#F8F1F8", "#077293"))
 
-    ha = HeatmapAnnotation(Subtype = meta[match(colnames(norm_df), meta$sample),]$subtype, 
+    ha = HeatmapAnnotation(Subtype = meta[match(colnames(df), meta$sample),]$subtype, 
                        col = list(Subtype = subtype_pal))
 
     filename <- paste0("data/results/figures/3-DataExploration/ARCHEheatmaps/", label, "_ARCHE_scores.png")
     png(filename, width = 8, height = 4, res = 600, units = "in")
     print(
-        Heatmap(norm_df, cluster_rows = FALSE, name = "ARCHE\nScore", col = score_pal,
-            column_title = "Samples", column_title_side = "bottom", column_names_gp = gpar(fontsize = 9), row_names_gp = gpar(fontsize = 10),
-            top_annotation = ha)
+        Heatmap(df, cluster_rows = FALSE, name = "ARCHE\nScore", col = score_pal,
+            column_title = "Samples", column_title_side = "bottom", column_names_gp = gpar(fontsize = 9), row_names_gp = gpar(fontsize = 10))
+            ,
+            #top_annotation = ha)
     )
     dev.off()
 }
 
 #' Compare subsetted and all sites
 #' 
-plot_ARCHE_scores_compare <- function(ps, pa, label, sample) {
+plot_ARCHE_scores_compare <- function(p2, p5, pa, sample) {
 
     # normalize
-    ps_norm <- znorm(ps)
+    p2_norm <- znorm(p2)
+    p5_norm <- znorm(p5)
     pa_norm <- znorm(pa)
 
     # ----- Plot unnormalized
 
     # format dataframes
-    ps$ARCHE <- sub("_.*", "", rownames(ps))
-    ps <- reshape2::melt(ps)
-    ps$Sites <- label
+    p2$ARCHE <- sub("_.*", "", rownames(p2))
+    p2 <- reshape2::melt(p2)
+    p2$Sites <- "20k sites"
+
+    p5$ARCHE <- sub("_.*", "", rownames(p5))
+    p5 <- reshape2::melt(p5)
+    p5$Sites <- "50k sites"
 
     pa$ARCHE <- sub("_.*", "", rownames(pa))
     pa <- reshape2::melt(pa)
     pa$Sites <- "All sites"
 
     # create toPlot
-    toPlot <- rbind(ps, pa)
+    toPlot <- rbind(p2, p5, pa)
     toPlot$ARCHE <- sub("ARCHE", "", toPlot$ARCHE)
 
-    filename <- paste0("data/results/figures/3-DataExploration/ARCHEheatmaps/", sample, "_", sub(" ", "_", label), "compare_unnorm.png")
+    filename <- paste0("data/results/figures/3-DataExploration/ARCHEheatmaps/", sample, "_compare_unnorm.png")
     png(filename, width = 4, height = 6, res = 600, units = "in")
     print(ggplot(toPlot, aes(x = ARCHE, y = variable, fill = value)) +
         geom_tile() +
@@ -69,19 +75,23 @@ plot_ARCHE_scores_compare <- function(ps, pa, label, sample) {
     # ----- Plot normalized (same code)
 
     # format dataframes
-    ps_norm$ARCHE <- sub("_.*", "", rownames(ps_norm))
-    ps <- reshape2::melt(ps_norm)
-    ps$Sites <- label
+    p2_norm$ARCHE <- sub("_.*", "", rownames(p2_norm))
+    p2 <- reshape2::melt(p2_norm)
+    p2$Sites <- "20k sites"
+
+    p5_norm$ARCHE <- sub("_.*", "", rownames(p5_norm))
+    p5 <- reshape2::melt(p5_norm)
+    p5$Sites <- "50k sites"
 
     pa_norm$ARCHE <- sub("_.*", "", rownames(pa_norm))
     pa <- reshape2::melt(pa_norm)
     pa$Sites <- "All sites"
 
     # create toPlot
-    toPlot <- rbind(ps, pa)
+    toPlot <- rbind(p2, p5, pa)
     toPlot$ARCHE <- sub("ARCHE", "", toPlot$ARCHE)
 
-    filename <- paste0("data/results/figures/3-DataExploration/ARCHEheatmaps/", sample, "_", sub(" ", "_", label), "compare_znorm.png")
+    filename <- paste0("data/results/figures/3-DataExploration/ARCHEheatmaps/", sample, "_compare_znorm.png")
     png(filename, width = 4, height = 6, res = 600, units = "in")
     print(ggplot(toPlot, aes(x = ARCHE, y = variable, fill = value)) +
         geom_tile() +
