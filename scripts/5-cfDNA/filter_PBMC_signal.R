828ac3bd72f9246ace857241968f7ee6b447c034
Julia Nguyen <julianguyenpt@gmail.com>
2025-11-26 17:10:35 -0500

    feat: blood diff basal and her2

--- scripts/5-cfDNA/filter_PBMC_signal.R
+++ scripts/5-cfDNA/filter_PBMC_signal.R
@@ -24,12 +24,24 @@ filename <- "Blood_enriched_vs_ER_FDR005_FOLD1.bed"
 blood_er <- read.table(paste0(dir, filename))
 colnames(blood_er) <- c("chr", "start", "end")
 
+# blood enriched vs Basal
+filename <- "Blood_enriched_vs_Basal_FDR005_FOLD1.bed"
+blood_basal <- read.table(paste0(dir, filename))
+colnames(blood_basal) <- c("chr", "start", "end")
+
+#blood enriched vs HER2
+filename <- "Blood_enriched_vs_Her2_FDR005_FOLD1.bed"
+blood_her2 <- read.table(paste0(dir, filename))
+colnames(blood_her2) <- c("chr", "start", "end")
+
 ###########################################################
 # Find hits in ARCHE beds
 ###########################################################
 
 filter_PBMC_signal(blood_er, "BloodvsER")
 filter_PBMC_signal(blood_all, "BloodvsAll")
+filter_PBMC_signal(blood_basal, "BloodvsBasal")
+filter_PBMC_signal(blood_her2, "BloodvsHer2")
 
 ###########################################################
 # TODO: compare sites filtered across different differntials
