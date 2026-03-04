c3fbcfeadafbf5c1abcb76916160eb77d6c4c7f6
Julia Nguyen <julianguyenpt@gmail.com>
2026-03-04 00:31:30 -0500

    feat: increase sample inclusion

--- scripts/Misc/grant_figure4.R
+++ scripts/Misc/grant_figure4.R
@@ -27,8 +27,8 @@ cicada_meta <- read.csv("data/rawdata/cfDNA/CICADA-ARCHE/sample_metadata.csv")
 cicada <- score_arche_cfDNA("CICADA-BloodvsER-25", meta = cicada_meta)
 arche_keep <- c(paste0("ARCHE", c(1, 3:6), "_50k"), "ARCHE2_20k")
 cicada <- cicada[cicada$Label %in% arche_keep,]
-cicada <- cicada[cicada$TF_group == "TF>10",]
-cicada <- cicada[complete.cases(cicada),]
+cicada$TF_group <- ifelse(cicada$TF > 5, "TF>5", "Not")
+cicada <- cicada[cicada$TF_group == "TF>5",]
 
 # get ARCHE scores for preclinical samples
 preclinical <- score_arche_cfDNA("preclinical-ARCHE")
@@ -42,19 +42,19 @@ preclinical$Sample[preclinical$Sample == "CAMA1_mouse_ctDNA"] <- "CAMA1_xeno"
 
 # add labels
 cicada$Sample_Type <- "Patient (CICADA)"
-cicada$ARCHE <- factor(cicada$ARCHE, levels = paste0("ARCHE", 6:1))
-cicada$plot_score <- log10((cicada$Score+0.01)*2) + 0.9
+cicada$ARCHE <- factor(cicada$ARCHE, levels = paste0("ARCHE", 1:6))
 cicada$pheno <- cicada_meta$pheno_id[match(cicada$Sample, cicada_meta$sample_id)]
 cicada$time <- cicada_meta$time_id[match(cicada$Sample, cicada_meta$sample_id)]
+cicada <- cicada[complete.cases(cicada),]
 
 filename <- "data/results/figures/Misc/cicada_scores_time.png"
 png(filename, width=6, height=4, units='in', res = 600, pointsize=80)
-ggplot(cicada, aes(x = ARCHE, y = plot_score, fill = time)) +
+ggplot(cicada, aes(x = ARCHE, y = Score, fill = time)) +
     geom_boxplot() + 
+    geom_hline(yintercept = 0, linetype = "dashed", color = "gray") +
     geom_jitter(aes(fill = time), shape = 21, stroke = 0.2,
                 position = position_jitterdodge(jitter.width = 0.2)) +
     scale_fill_manual("Sample Type", values = c("#73937E", "#CEB992")) +
-    scale_y_continuous(limits = c(-0.15, 0.85)) +
     theme_minimal() +
     theme(
         axis.title.x = element_blank(),
