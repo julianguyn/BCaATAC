784eaa57c06ba23dd09678a0eec23b8fa9095970
Julia Nguyen <julianguyenpt@gmail.com>
2026-04-20 18:40:07 -0400

    feat: test drug response with deviations

---
+++ scripts/4-DrugResponse/ARCHE_deviations.R
@@ -0,0 +1,278 @@
+# try scoring drug response to deviation scores instead of zscores
+
+# load libraries
+suppressPackageStartupMessages({
+    library(PharmacoGx)
+    library(survcomp)
+    library(wesanderson)
+    library(ggplot2)
+    library(ggh4x)
+    library(reshape2)
+    library(meta)
+    library(ggpubr)
+    library(grid)
+    library(gridExtra)
+    library(dplyr)
+    library(readxl)
+    library(data.table)
+})
+
+source("utils/get_data.R")
+source("utils/mappings.R")
+source("utils/compute_drug_response.R")
+source("utils/plots/drug_response_ccls.R")
+source("utils/palettes.R")
+source("utils/bca_drugs.R")
+source("utils/plots/ARCHE_scores_heatmap.R")
+
+
+###########################################################
+# Load in data
+###########################################################
+
+# read in cell metadata
+meta <- read.csv("metadata/lupien_metadata.csv")
+
+# remove nergiz dups
+dups <- meta$sampleid[duplicated(meta$sampleid)]
+meta <- meta[!(meta$sampleid %in% dups & meta$tech == "nergiz"), ]
+
+# remove komal dups
+dups <- meta$sampleid[duplicated(meta$sampleid)]
+meta <- meta[!(meta$sampleid %in% dups & meta$tech == "tina"), ]
+
+c_meta <- meta[meta$type == "cell_line", ]
+
+# load in arche scores
+cells_20k <- get_arche_devs("cells", "20k", c_meta)
+cells_50k <- get_arche_devs("cells", "50k", c_meta)
+cells_all <- get_arche_devs("cells", "all", c_meta)
+
+# get drug sensitivity data
+load("data/procdata/CCLs/sensitivity_data.RData")
+
+###########################################################
+# Normalize ARCHE scores
+###########################################################
+
+norm_20k <- znorm(cells_20k)
+norm_50k <- znorm(cells_50k)
+norm_all <- znorm(cells_all)
+
+###########################################################
+# Compute PC of ARCHE-drug associations
+###########################################################
+
+# helper function to compute arche associations across all psets
+arche_pc <- function(scores) {
+    ubr1_PC <- compute_pc(scores, ubr1_sen, "UBR1")
+    ubr2_PC <- compute_pc(scores, ubr2_sen, "UBR2")
+    gray_PC <- compute_pc(scores, gray_sen, "GRAY")
+    gcsi_PC <- compute_pc(scores, gcsi_sen, "gCSI")
+    gdsc_PC <- compute_pc(scores, gdsc_sen, "GDSC2")
+    ctrp_PC <- compute_pc(scores, ctrp_sen, "CTRP")
+    ccle_PC <- compute_pc(scores, ccle_sen, "CCLE")
+
+    # compile results
+    PC_res <- rbind(ubr1_PC, ubr2_PC, gray_PC, gcsi_PC, gdsc_PC, ctrp_PC, ccle_PC)
+    return(PC_res)
+}
+
+pc_20k <- arche_pc(cells_20k)
+pc_50k <- arche_pc(cells_50k)
+pc_all <- arche_pc(cells_all)
+
+pcnorm_20k <- arche_pc(norm_20k)
+pcnorm_50k <- arche_pc(norm_50k)
+pcnorm_all <- arche_pc(norm_all)
+
+# save results
+save(pc_20k, pc_50k, pc_all, pcnorm_20k, pcnorm_50k, pcnorm_all,
+     file = "data/results/data/4-DrugResponse/CCLs/ARCHE_CCLs_deviations_associations.RData")
+
+###########################################################
+# Identify Class A Biomarkers
+###########################################################
+
+# helper function to identify Class A biomarkers
+get_classA <- function(PC_res, label) {
+    # Class A biomarkers: abs(PCC > 0.5) & FDR < 0.05 in 1 PSet
+    ClassA <- PC_res[which((abs(PC_res$pc) >= 0.5) & PC_res$FDR < 0.05),]
+    ClassA <- ClassA[order(ClassA$pc, decreasing = T),]
+    ClassA$rank <- factor(1:nrow(ClassA), levels = c(1:nrow(ClassA)))
+
+    # get Class A associations across PSets
+    toPlot <- PC_res[PC_res$pairs %in% ClassA$pairs,]
+    keep <- names(table(toPlot$pair)[table(toPlot$pair)>1])
+    toPlot <- toPlot[toPlot$pair %in% keep,]
+
+    # save Class A biomarkers
+    filepath <- paste0("data/results/data/4-DrugResponse/CCLs/ClassA_Biomarkers_", label, ".csv")
+    write.csv(ClassA, file = filepath, quote = FALSE, row.names = FALSE)
+    filepath <- paste0("data/results/data/4-Drugresponse/CCLs/ClassA_allAssociations_", label, ".csv")
+    write.csv(toPlot, file = filepath, quote = FALSE, row.names = FALSE)
+
+    # plot Class A heatmap (drug in >1 PSet)
+    plot_ClassA_heatmap(toPlot, "Multi", label)
+
+    # plot Class A associations in 1 PSet
+    toPlot <- ClassA[-which(ClassA$pairs %in% toPlot$pairs),]
+    toPlot <- map_drugs(toPlot)         # deal w drug names
+    plot_ClassA_heatmap(toPlot, "Single", label)
+
+    return(toPlot)
+}
+
+classA_20k <- get_classA(pc_20k, "20k")
+classA_50k <- get_classA(pc_50k, "50k")
+classA_all <- get_classA(pc_all, "all")
+
+classA_norm20k <- get_classA(pcnorm_20k, "norm_20k")
+classA_norm50k <- get_classA(pcnorm_50k, "norm_50k")
+classA_normall <- get_classA(pcnorm_all, "norm_all")
+
+###########################################################
+# Indiv plots for associations of interest
+###########################################################
+
+# helper function to plot individual plots
+indiv_plots <- function(pair, pcc) {
+
+    # unnormalized ARCHE scores
+    plot_indivPlot(pair, cells_20k, "cells_20k")
+    plot_indivPlot(pair, cells_50k, "cells_50k")
+    plot_indivPlot(pair, cells_all, "cells_all")
+
+    # normalized ARCHE scores
+    plot_indivPlot(pair, norm_20k, "cells_norm20k")
+    plot_indivPlot(pair, norm_50k, "cells_norm50k")
+    plot_indivPlot(pair, norm_all, "cells_normall")
+}
+
+# create dataframe to store results
+pcc <- data.frame(matrix(nrow=0, ncol=5))
+colnames(pcc) <- c("ARCHE_Drug", "Label", "PSet", "PCC", "pvalue")
+
+# plot for pairs of interest
+indiv_plots("ARCHE4_Etoposide")
+indiv_plots("ARCHE6_Etoposide")
+
+indiv_plots("ARCHE2_Paclitaxel")
+indiv_plots("ARCHE5_Paclitaxel")
+indiv_plots("ARCHE6_Paclitaxel")
+
+indiv_plots("ARCHE4_945")
+
+bcl2 <- c(
+    "decitabine:navitoclax (2:1 mol/mol)",
+    "doxorubicin:navitoclax (2:1 mol/mol)",
+    "alisertib:navitoclax (2:1 mol/mol)",
+    "navitoclax:MST-312 (1:1 mol/mol)",
+    "selumetinib:navitoclax (8:1 mol/mol)"
+)
+pairs <- paste0("ARCHE2_", bcl2)
+for (pair in pairs) {
+    indiv_plots(pair)
+}
+indiv_plots("ARCHE5_navitoclax:birinapant (1:1 mol/mol)")
+
+indiv_plots("ARCHE1_Olaparib")
+
+indiv_plots("ARCHE5_Simvastatin")
+indiv_plots("ARCHE4_Simvastatin")
+indiv_plots("ARCHE6_Simvastatin")
+indiv_plots("ARCHE6_Lovastatin")
+indiv_plots("ARCHE2_Simvastatin")
+indiv_plots("ARCHE2_Lovastatin")
+indiv_plots("ARCHE3_Simvastatin")
+
+
+write.csv(pcc, file = "data/results/data/4-DrugResponse/CCLs/indiv_PCC.csv", quote = FALSE, row.names = FALSE)
+
+sig <- pcc[abs(pcc$PCC) > 0.4 & pcc$pvalue < 0.1,]
+write.csv(sig, file = "data/results/data/4-DrugResponse/CCLs/indiv_PCC_sig.csv", quote = FALSE, row.names = FALSE)
+
+###########################################################
+# Identify Class B Biomarkers
+###########################################################
+
+# helper function to compile PCC and meta-estimates for ClassB biomarkers
+get_classB <- function(PC_res, label) {
+
+    # perform meta analysis and save results
+    estimates <- compute_meta(PC_res)
+    filepath <- paste0("data/results/data/4-DrugResponse/CCLs/meta_estimates", label, ".csv")
+    write.csv(estimates, file = filepath, quote = FALSE, row.names = FALSE)
+
+    #ClassB biomarkers: abs(TE > 0.4) & FDR < 0.05
+    ClassB <- estimates[which(abs(estimates$TE) > 0.4 & estimates$FDR < 0.05),]
+    ClassB <- ClassB[order(ClassB$TE, decreasing = TRUE),]
+    ClassB$rank <- factor(1:nrow(ClassB), levels = c(1:nrow(ClassB)))
+    
+    keep <- PC_res[which(PC_res$pairs %in% ClassB$pair),]
+    
+    # combine PC_res and meta results
+    toPlot <- data.frame(
+        signature = c(keep$signature, ClassB$signature),
+        drug = c(keep$drug, ClassB$drug),
+        pairs = c(keep$pairs, ClassB$pair),
+        estimate = c(keep$pc, ClassB$TE),
+        upper = c(keep$upper, ClassB$upper),
+        lower = c(keep$lower, ClassB$lower),
+        FDR = c(keep$FDR, ClassB$FDR),
+        pset = c(keep$pset, rep("Meta Estimate", nrow(ClassB)))
+    )
+    toPlot$meta <- factor(ifelse(toPlot$pset == "Meta Estimate", TRUE, FALSE), levels = c(TRUE, FALSE))
+    toPlot$pset <- factor(toPlot$pset, levels = c(unique(PC_res$pset), "Meta Estimate"))
+
+    # plot Class B biomarker associations as forest plot
+    #plot_ClassB_forest(toPlot, label)
+
+    # plot Class B biomarker associations as heatmap
+    plot_ClassB_heatmap(toPlot, label)
+
+    return(toPlot)
+}
+
+classB_20k <- get_classB(pc_20k, "20k")
+classB_50k <- get_classB(pc_50k, "50k")
+classB_all <- get_classB(pc_all, "all")
+
+classB_norm20k <- get_classB(pcnorm_20k, "norm20k")
+classB_norm50k <- get_classB(pcnorm_50k, "norm50k")
+classB_normall <- get_classB(pcnorm_all, "normall")
+
+
+###########################################################
+# Combine TDXd data for plotting
+###########################################################
+
+# helper function to combine drug response and arche scores
+combine_data <- function(signature_scores, tdxd) {
+    combined <- t(rbind(tdxd, signature_scores)) |> as.data.frame()
+    samples <- rownames(combined)
+    combined <- sapply(combined, as.numeric) |> as.data.frame()
+    rownames(combined) <- samples
+    combined$subtype <- true_subtype$subtype[match(samples, true_subtype$sample)]
+    return(combined)
+}
+
+all_c <- combine_data(tdxd_sig, tdxd)
+her_c <- combine_data(her2, her2_tdxd)
+non_c <- combine_data(nonher2, nonher2_tdxd)
+erbb2_c <- combine_data(erbb2, erbb2_tdxd)
+
+###########################################################
+# Plot correlation of ARCHE and TDXd response
+###########################################################
+
+plot_tdxd_all(all_c, tdxd_PC, "Avg.IC50", "All Cells")
+plot_tdxd_all(all_c, tdxd_PC, "Avg.IC50.Treps", "All Cells")
+plot_tdxd_all(her_c, tdxd_PC, "Avg.IC50", "HER2 Cells")
+plot_tdxd_all(her_c, tdxd_PC, "Avg.IC50.Treps", "HER2 Cells")
+plot_tdxd_all(non_c, tdxd_PC, "Avg.IC50", "Non-HER2 Cells")
+plot_tdxd_all(non_c, tdxd_PC, "Avg.IC50.Treps", "Non-Her2 Cells")
+
+plot_tdxd_corr(erbb2_c, erbb2_PC, "ENSG00000141736", "Avg.IC50", save = TRUE)
+plot_tdxd_corr(erbb2_c, erbb2_PC, "ENSG00000141736", "Avg.IC50.Treps", save = TRUE)
+ 
