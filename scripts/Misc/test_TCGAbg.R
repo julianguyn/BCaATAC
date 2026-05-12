2d36660fcedc390a867dd8fd6ace661da74f51d5
Julia Nguyen <julianguyenpt@gmail.com>
2026-05-12 13:32:45 -0400

    feat: plot pc123

--- scripts/Misc/test_TCGAbg.R
+++ scripts/Misc/test_TCGAbg.R
@@ -1,8 +1,13 @@
 # umap - run on H4H
 
 library(data.table)
+library(ggplot2)
+
+set.seed(101)
+source("utils/palettes.R")
 
 INDIR <- "/cluster/projects/bhklab/projects/BCaATAC/BCa_ARCHE_Scoring/data/results/"
+OUTDIR <- "data/results/figures/Misc/scoring/"
 
 ###########################################################
 # Load in data
@@ -75,7 +80,7 @@ run_pca <- function(merged, outfile) {
   variance_pca / sum(variance_pca) -> prop_var
   print(head(prop_var))
 
-  pca_res <- pca_res$x[,c(1:2)]
+  pca_res <- pca_res$x[,c(1:3)]
   print(head(pca_res))
 
   save(pca_res, samples, prop_var, file = outfile)
@@ -86,12 +91,75 @@ run_pca(merged_cells, "cell_tcga_pca.RData")
 run_pca(merged_pdxs, "pdx_tcga_pca.RData")
 
 ###########################################################
+# Load in metadata
+###########################################################
+
+# read in sample metadata
+meta <- read.csv("metadata/lupien_metadata.csv")
+
+# read in tcga sample matching
+mapping <- read.csv("data/rawdata/TCGA/TCGA_sourcefiles.csv")
+mapping$Sample.Name <- gsub("\\.", "-", mapping$Sample.Name)
+mapping$ATAC.Seq.File.Name <- gsub("\\.", "-", mapping$ATAC.Seq.File.Name)
+
+###########################################################
+# Helper function to plot PCA plots
+###########################################################
+
+plot_pca <- function(label) {
+
+  # plot eigenvalues
+  filename <- paste0(OUTDIR, label, "_tcga_eigen.png")
+  png(filename, width = 6, height = 4, res = 600, units = "in")
+  plot(prop_var,
+      xlab = "Principal Components",
+      ylab = "Proportion of Variance Explained",
+      ylim = c(0, 1))
+  dev.off()
+
+  # add labels to pca
+  pca_res <- as.data.frame(pca_res)
+  for (i in 1:nrow(pca_res)) {
+    if (rownames(pca_res)[i] %in% meta$filename) {
+      pca_res$Sample_Type[i] <- label
+      pca_res$Subtype[i] <- meta$subtype[meta$filename == rownames(pca_res)[i]]
+      pca_res$Tech[i] <- meta$tech[meta$filename == rownames(pca_res)[i]]
+    } else {
+      pca_res$Sample_Type[i] <- "tumour"
+      pca_res$Subtype[i] <- mapping$Subtype[mapping$ATAC.Seq.File.Name == rownames(pca_res)[i]]
+      pca_res$Tech[i] <- "TCGA"
+    }
+  }
+  pca_res$Subtype[is.na(pca_res$Subtype)] <- "unknown"
+
+  # plot PC1vs2
+  p <- ggplot(pca_res, aes(x = PC1, y = PC2, shape = Sample_Type, fill = Subtype)) +
+    geom_point(size = 3, alpha = 0.8) +
+    scale_fill_manual(values = subtype_pal) +
+    scale_shape_manual(values = c(24, 21)) +
+    theme_bw() +
+    guides(fill = guide_legend(override.aes = list(shape = 21)))
+  filename <- paste0(OUTDIR, label, "_tcga_pca12.png")
+  ggsave(filename, p, width = 7, height = 5)
+
+  # plot PC2vs3
+  p <- ggplot(pca_res, aes(x = PC1, y = PC2, shape = Sample_Type, fill = Subtype)) +
+    geom_point(size = 3, alpha = 0.8) +
+    scale_fill_manual(values = subtype_pal) +
+    scale_shape_manual(values = c(24, 21)) +
+    theme_bw() +
+    guides(fill = guide_legend(override.aes = list(shape = 21)))
+  filename <- paste0(OUTDIR, label, "_tcga_pca23.png")
+  ggsave(filename, p, width = 7, height = 5)
+
+}
+
+###########################################################
 # Plot PCA
 ###########################################################
 
 load("data/procdata/scoring/cell_tcga_pca.RData")
+plot_pca("cell")
 
-plot(prop_var,
-     xlab = "Principal Components",
-     ylab = "Proportion of Variance Explained",
-     ylim = c(0, 1))
+load("data/procdata/scoring/pdx_tcga_pca.RData")
+plot_pca("pdx")
