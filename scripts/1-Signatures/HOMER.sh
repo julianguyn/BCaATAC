35409ec21d93ac0b5a4a2b318c73f984438847cf
Julia Nguyen <julianguyenpt@gmail.com>
2026-06-30 18:19:08 -0400

    feat: rerun on 50k

--- scripts/1-Signatures/HOMER.sh
+++ scripts/1-Signatures/HOMER.sh
@@ -7,32 +7,33 @@
 #SBATCH --job-name=runHOMER
 #SBATCH --output=logs/runHOMER.out
 
+cd /cluster/projects/bhklab/projects/BCaATAC/HOMER/scripts
 
 ###########################################################
 # Specify genome and annotation file
 ###########################################################
 
-GENOME="../references/GENCODE/GRCh38_v45/genome.fa"
-GTF="../references/GENCODE/GRCh38_v45/annotation.gtf"
+GENOME="../references/GENCODE/human/GRCh38_v45/genome.fa"
+GTF="../references/GENCODE/human/GRCh38_v45/annotation.gtf"
 
 ###########################################################
 # Annotate ARCHE peak sets
 ###########################################################
 
-annotatePeaks.pl ../data/ARCHE1_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE1.txt
-annotatePeaks.pl ../data/ARCHE2_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE2.txt
-annotatePeaks.pl ../data/ARCHE3_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE3.txt
-annotatePeaks.pl ../data/ARCHE4_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE4.txt
-annotatePeaks.pl ../data/ARCHE5_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE5.txt
-annotatePeaks.pl ../data/ARCHE6_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE6.txt
+#annotatePeaks.pl ../data/ARCHE1_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE1.txt
+#annotatePeaks.pl ../data/ARCHE2_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE2.txt
+#annotatePeaks.pl ../data/ARCHE3_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE3.txt
+#annotatePeaks.pl ../data/ARCHE4_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE4.txt
+#annotatePeaks.pl ../data/ARCHE5_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE5.txt
+#annotatePeaks.pl ../data/ARCHE6_20k.bed $GENOME -gtf $GTF -size given > ../results/annotatePeaks/ARCHE6.txt
 
 ###########################################################
 # Motif enrichment
 ###########################################################
 
-findMotifsGenome.pl ../data/ARCHE1_20k.bed $GENOME ../results/findMotifsGenome/ARCHE1 -bg ../data/Background.bed -size given
-findMotifsGenome.pl ../data/ARCHE2_20k.bed $GENOME ../results/findMotifsGenome/ARCHE2 -bg ../data/Background.bed -size given
-findMotifsGenome.pl ../data/ARCHE3_20k.bed $GENOME ../results/findMotifsGenome/ARCHE3 -bg ../data/Background.bed -size given
-findMotifsGenome.pl ../data/ARCHE4_20k.bed $GENOME ../results/findMotifsGenome/ARCHE4 -bg ../data/Background.bed -size given
-findMotifsGenome.pl ../data/ARCHE5_20k.bed $GENOME ../results/findMotifsGenome/ARCHE5 -bg ../data/Background.bed -size given
-findMotifsGenome.pl ../data/ARCHE6_20k.bed $GENOME ../results/findMotifsGenome/ARCHE6 -bg ../data/Background.bed -size given
+/cluster/home/julian/BCaATAC/HOMER/bin/findMotifsGenome.pl ../data/ARCHE1_50k.bed $GENOME ../results/findMotifsGenome/ARCHE1 -bg ../data/Background.bed -size given
+/cluster/home/julian/BCaATAC/HOMER/bin/findMotifsGenome.pl ../data/ARCHE2_50k.bed $GENOME ../results/findMotifsGenome/ARCHE2 -bg ../data/Background.bed -size given
+/cluster/home/julian/BCaATAC/HOMER/bin/findMotifsGenome.pl ../data/ARCHE3_50k.bed $GENOME ../results/findMotifsGenome/ARCHE3 -bg ../data/Background.bed -size given
+/cluster/home/julian/BCaATAC/HOMER/bin/findMotifsGenome.pl ../data/ARCHE4_50k.bed $GENOME ../results/findMotifsGenome/ARCHE4 -bg ../data/Background.bed -size given
+/cluster/home/julian/BCaATAC/HOMER/bin/findMotifsGenome.pl ../data/ARCHE5_50k.bed $GENOME ../results/findMotifsGenome/ARCHE5 -bg ../data/Background.bed -size given
+/cluster/home/julian/BCaATAC/HOMER/bin/findMotifsGenome.pl ../data/ARCHE6_50k.bed $GENOME ../results/findMotifsGenome/ARCHE6 -bg ../data/Background.bed -size given
